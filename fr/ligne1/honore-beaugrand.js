2414e13c6240e913e3ff31e5b8e9b02af0cba750
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-04-17 16:59:31 -0400

    Update honore-beaugrand.js

--- fr/ligne1/honore-beaugrand.js
+++ fr/ligne1/honore-beaugrand.js
@@ -1,13 +1,14 @@
 function init()
 {
-	hnames = [ "Village Champlain", "Place Versailles", "Terminus Honor&eacute;-Beaugrand", "Parc Li&eacute;bert", "&Eacute;cole Sainte-Louise-de-Marillac", "Parc Michel-Bourdon", "Manoir Claudette Barr&eacute;" ];
+	hnames = [ "Village Champlain", "Place Versailles", "Terminus Honor&eacute;-Beaugrand", "Parc Li&eacute;bert", "&Eacute;cole Sainte-Louise-de-Marillac", "Parc Michel-Bourdon", "Manoir Claudette Barr&eacute;",
+		 "Parc Dup&eacute;r&eacute;", "Centre d&#39;H&eacute;bergement Jean-Hubert-Biermans", "Centre d&#39;H&eacute;bergement Pierre-Joseph-Triest" ];
 	
-	htypes = [ "Magasinage", "Centre Commercial", "Terminus d&#39;Autobus", "Parc", "&Eacute;cole", "Parc", "R&eacute;sidence" ];
+	htypes = [ "Magasinage", "Centre Commercial", "Terminus d&#39;Autobus", "Parc", "&Eacute;cole", "Parc", "R&eacute;sidence", "Parc", "R&eacute;sidence", "R&eacute;sidence" ];
 	
-	haddresses = [ "7788 Rue Sherbrooke Est", "7275 Rue Sherbrooke Est", "4755 Rue Honor&eacute;-Beaugrand", "2848 Rue Li&eacute;bert", "8100 Rue de Marseille", "4507 Rue Joseph-A.-Rodier", "7979 Rue Sherbrooke Est" ];
+	haddresses = [ "7788 Rue Sherbrooke Est", "7275 Rue Sherbrooke Est", "4755 Rue Honor&eacute;-Beaugrand", "2848 Rue Li&eacute;bert", "8100 Rue de Marseille", "4507 Rue Joseph-A.-Rodier", "7979 Rue Sherbrooke Est", "4404 Rue Saint-Donat", "7905 Rue Sherbrooke Est", "4900 Boulevard Lapointe" ];
 	
 	hsites = [ "#", "https://www.placeversailles.com/fr", "https://exo.quebec/fr/planifier-trajet/station-honore-beaugrand", "https://montreal.ca/lieux/parc-liebert", "https://ste-louise-de-marillac.csdm.ca/", "https://montreal.ca/lieux/parc-michel-bourdon",
-	"https://www.manoirclaudettebarre.ca/" ];
+	"https://www.manoirclaudettebarre.ca/", "https://montreal.ca/lieux/parc-dupere", "https://ciusss-estmtl.gouv.qc.ca/etablissement/chsld-jean-hubert-biermans", "https://ciusss-estmtl.gouv.qc.ca/etablissement/chsld-pierre-joseph-triest" ];
 	
 	if(hnames.length == 1){
 		document.getElementById("hotspot-js").innerHTML += "<span style=\"font-size: 14pt\"><em lang=\"fr\"><b>"+hnames.length+ " point d&#39;int&eacute;r&ecirc;t est &agrave; proximit&eacute; de cette station.</b></em></span><br/><br/>";
