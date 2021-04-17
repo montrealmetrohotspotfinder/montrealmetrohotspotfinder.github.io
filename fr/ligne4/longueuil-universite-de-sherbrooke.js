661c2e14da893c77b775a1a68a1f02dbcaae460d
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-04-17 16:09:07 -0400

    Update longueuil-universite-de-sherbrooke.js

--- fr/ligne4/longueuil-universite-de-sherbrooke.js
+++ fr/ligne4/longueuil-universite-de-sherbrooke.js
@@ -12,9 +12,10 @@ function init()
 	"Axc&egrave;s Saint-Charles",
 	"L&#39;Estuaire III",
 	"Le D&#39;Assigny",
-	"Revenu Qu&eacute;bec, bureaux longueuillois" ];
+	"Revenu Qu&eacute;bec, bureaux longueuillois",
+	"Tribunal Administratif du Logement du Qu&eacute;bec, bureaux longueuillois" ];
 	
-	htypes = [ "Terminus d&#39;Autobus", "Universit&eacute;", "Universit&eacute;", "H&ocirc;tel", "H&ocirc;tel", "Centre Commercial", "A&eacute;roport", "Coll&egrave;ge", "Agence", "R&eacute;sidence", "R&eacute;sidence", "R&eacute;sidence", "Agence" ];
+	htypes = [ "Terminus d&#39;Autobus", "Universit&eacute;", "Universit&eacute;", "H&ocirc;tel", "H&ocirc;tel", "Centre Commercial", "A&eacute;roport", "Coll&egrave;ge", "Agence", "R&eacute;sidence", "R&eacute;sidence", "R&eacute;sidence", "Agence", "Agence" ];
 	
 	haddresses = [ "120 Place Charles-Lemoyne", 
 	"150 Place Charles-Lemoyne", 
@@ -28,7 +29,8 @@ function init()
 	"1155 Rue Saint-Charles Ouest",
 	"70 Rue de la Barre",
 	"110 Rue de la Barre",
-	"825 Rue Saint-Laurent Ouest" ];
+	"825 Rue Saint-Laurent Ouest",
+	"201 Place Charles-Lemoyne" ];
 	
 	hsites = [ "https://exo.quebec/fr/planifier-trajet/installations-intermodales/terminus-longueuil", 
 	"http://www.usherbrooke.ca/longueuil/", 
@@ -42,7 +44,8 @@ function init()
 	"https://habitationstrigone.com/dwellings/axces-saint-charles/",
 	"http://estuaire3.ca/",
 	"https://groupemercille.com/fr/residentiel/3-12",
-	"https://www.revenuquebec.ca/fr/" ];
+	"https://www.revenuquebec.ca/fr/",
+	"https://www.tal.gouv.qc.ca/" ];
 	
 	if(hnames.length == 1){
 		document.getElementById("hotspot-js").innerHTML += "<span style=\"font-size: 14pt\"><em lang=\"fr\"><b>"+hnames.length+ " point d&#39;int&eacute;r&ecirc;t est &agrave; proximit&eacute; de cette station.</b></em></span><br/><br/>";
