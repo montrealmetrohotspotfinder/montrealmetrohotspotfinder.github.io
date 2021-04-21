ac27f5706177417d85f22a680fd0e84c45fcef7d
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-04-21 16:28:28 -0400

    Update acadie.js

--- en/line5/acadie.js
+++ en/line5/acadie.js
@@ -2,7 +2,8 @@ function init()
 {
 	spots = [{name: "Altasciences", type: "Pharmacy", address: "1100 Avenue Beaumont", site: "https://www.altasciences.com/"},
 	{name: "March&eacute; Central via Bus 179", type: "Mall", address: "9187 Boulevard de l&#39;Acadie", site: "https://www.marchecentral.com/en"},
-	{name: "Parc Hamilton", type: "Park", address: "", site: "#"}];
+	{name: "Parc Hamilton", type: "Park", address: "", site: "#"},
+	{name: "Universit&eacute; de Montr&eacute;al, MIL campus", type: "University", address: "1375 Avenue Th&eacute;r&egrave;se-Lavoie-Roux", site: "https://campusmil.umontreal.ca/"}];
 	
 	if(spots.length == 1){
 		document.getElementById("hotspot-js").innerHTML += "<span style=\"font-size: 14pt\"><b>"+spots.length+ " hotspot is near this station.</b></span><br/><br/>";
