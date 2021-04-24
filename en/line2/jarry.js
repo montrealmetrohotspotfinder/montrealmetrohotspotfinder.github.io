c5cde44a89882bd0000b17e87ac0ee6de8a3ea2d
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-04-23 22:37:51 -0400

    Update jarry.js

--- en/line2/jarry.js
+++ en/line2/jarry.js
@@ -5,7 +5,8 @@ function init()
 	{name: "Parc Saint-Vincent-Ferrier", type: "Park", address: "8172 Rue Drolet", site: "https://montreal.ca/en/places/parc-saint-vincent-ferrier"},
 	{name: "Parc G&eacute;rard-Lalonde", type: "Park", address: "375 Rue Jarry Est", site: "#"},
 	{name: "Paroisse Saint-Vincent-Ferrier", type: "Church", address: "301 Rue Jarry Est", site: "https://diocesemontreal.org/en/lets-connect/churches-parishes/saint-vincent-ferrier"},
-	{name: "&Eacute;cole H&eacute;l&egrave;ne-Boull&eacute;", type: "School", address: "8050 Avenue de Gasp&eacute;", site: "https://helene-boulle.cssdm.gouv.qc.ca/"}];
+	{name: "&Eacute;cole H&eacute;l&egrave;ne-Boull&eacute;", type: "School", address: "8050 Avenue de Gasp&eacute;", site: "https://helene-boulle.cssdm.gouv.qc.ca/"},
+	{name: "Centre Sainte-Croix", type: "School", address: "85 Rue Jarry Ouest", site: "https://centre-ste-croix.cssdm.gouv.qc.ca/"}];
 		
 	if(spots.length == 1){
 		document.getElementById("hotspot-js").innerHTML += "<span style=\"font-size: 14pt\"><b>"+spots.length+ " hotspot is near this station.</b></span><br/><br/>";
