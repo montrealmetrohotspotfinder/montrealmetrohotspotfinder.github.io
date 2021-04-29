cb11c98cccb3c950afe7271daa496083c019824b
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-04-29 04:26:45 -0400

    Update recherche.js

--- fr/recherche.js
+++ fr/recherche.js
@@ -1113,16 +1113,17 @@ function init(){
 	
 	Sherbrookenames = [ "Institut de Tourisme et d&#39;H&ocirc;tellerie du Qu&eacute;bec", "H&ocirc;tel de l&#39;ITHQ", "Parc La Fontaine", "Square Saint-Louis", "Auberge du Carr&eacute; Saint-Louis", 
 	"Th&eacute;&acirc;tre d&#39;Aujourd&#39;hui", "Th&eacute;&acirc;tre de Quat&#39;Sous", "Th&eacute;&acirc;tre La Chappelle", "Les R&eacute;sidences Soleil Manoir Plaza",
-	"Gare d&#39;Autocars de Montr&eacute;al", "Quartier Latin", "Champlain College (Vermont), campus Montr&eacute;al", "Parc Jehane-Beno&icirc;t", "Parc J.-Z.-L&eacute;on-Patenaude", "C&eacute;gep du Vieux Montr&eacute;al" ];
+	"Gare d&#39;Autocars de Montr&eacute;al", "Quartier Latin", "Champlain College (Vermont), campus Montr&eacute;al", "Parc Jehane-Beno&icirc;t", "Parc J.-Z.-L&eacute;on-Patenaude", "C&eacute;gep du Vieux Montr&eacute;al", "Loft Hotel Montr&eacute;al" ];
 	
-	Sherbrooketypes = [ "Universit&eacute;", "H&ocirc;tel", "Parc", "Place de la Ville", "H&ocirc;tel", "Th&eacute;&acirc;tre", "Th&eacute;&acirc;tre", "Th&eacute;&acirc;tre", "R&eacute;sidence", "Terminus d&#39;Autobus", "District", "Coll&egrave;ge", "Parc", "Parc", "Coll&egrave;ge" ];
+	Sherbrooketypes = [ "Universit&eacute;", "H&ocirc;tel", "Parc", "Place de la Ville", "H&ocirc;tel", "Th&eacute;&acirc;tre", "Th&eacute;&acirc;tre", "Th&eacute;&acirc;tre", "R&eacute;sidence", "Terminus d&#39;Autobus", "District", "Coll&egrave;ge", "Parc", "Parc", "Coll&egrave;ge", "H&ocirc;tel" ];
 	
 	Sherbrookeaddresses = [ "3535 Rue Saint-Denis", "3535 Rue Saint-Denis", "3819 Avenue Calixa-Lavall&eacute;e", "", "3466 Rue Saint-Denis", "3900 Rue Saint-Denis", "100 Avenue des Pins Est", "3700 Rue Saint-Dominique",
-	"505 Rue Sherbrooke Est", "1717 Rue Berri", "", "525 Rue Sherbrooke Est", "", "", "255 Rue Ontario Est" ];
+	"505 Rue Sherbrooke Est", "1717 Rue Berri", "", "525 Rue Sherbrooke Est", "", "", "255 Rue Ontario Est", "334 Terrasse Saint Denis" ];
 	
 	Sherbrookesites = [ "https://www.ithq.qc.ca/institut/", "http://www.ithq.qc.ca/hotel/", "https://montreal.ca/lieux/parc-la-fontaine", "https://montreal.ca/lieux/square-saint-louis", "https://aubergecarrestlouis.com/Accueil",
 	"https://www.theatredaujourdhui.qc.ca/", "https://www.quatsous.com/", "https://lachapelle.org/fr", "https://residencessoleil.ca/liste-des-residences-soleil/manoir-plaza", "http://www.gamtl.com/fr/bienvenue/default.aspx", 
-	"http://www.quartierlatin.ca/fr/", "https://montreal.champlain.edu/", "https://montreal.ca/lieux/parc-jehane-benoit", "https://montreal.ca/lieux/parc-j-z-leon-patenaude", "http://www.cvm.qc.ca/Pages/index.aspx" ];
+	"http://www.quartierlatin.ca/fr/", "https://montreal.champlain.edu/", "https://montreal.ca/lieux/parc-jehane-benoit", "https://montreal.ca/lieux/parc-j-z-leon-patenaude", "http://www.cvm.qc.ca/Pages/index.aspx",
+	"https://lofthotel.guestybookings.com/" ];
 	
 	ChampDeMarsnames = [ "Vieux Montr&eacute;al", "Vieux Port de Montr&eacute;al", "Place des Montr&eacute;alaises", "CHUM Saint-Luc", "Quartier Chinois", "Place Vauquelin", "Champ de Mars", "H&ocirc;tel de Ville de Montr&eacute;al", "&Eacute;difice Lucien-Saulnier", "Ch&acirc;teau Ramezay",
 	"March&eacute; Bonsecours", "Terrasses Bonsecours", "Chappelle Notre-Dame-de-Bon-Secours", "Place Jacques-Cartier", "Quai Jacques-Cartier", "Quai de l&#39;Horloge", "Bassin Bonsecours", 
