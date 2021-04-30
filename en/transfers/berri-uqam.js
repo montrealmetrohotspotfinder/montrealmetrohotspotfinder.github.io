ef84ce95c7afd683fc2932c915a145e1caaeb125
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-04-30 16:03:17 -0400

    Update berri-uqam.js

--- en/transfers/berri-uqam.js
+++ en/transfers/berri-uqam.js
@@ -35,7 +35,8 @@ function init()
 	{name: "Universit&eacute; du Qu&eacute;bec &agrave; Montr&eacute;al, Th&eacute;r&egrave;se-Casgrain Pavilion", type: "University", address: "455 Boulevard Ren&eacute;-L&eacute;vesque Est", site: "https://carte.uqam.ca/pavillon-w"},
 	{name: "Universit&eacute; du Qu&eacute;bec &agrave; Montr&eacute;al, Paul-G&eacute;rin-Lajoie Pavilion", type: "University", address: "1205 Rue Saint-Denis", site: "https://carte.uqam.ca/pavillon-n"},
 	{name: "Universit&eacute; du Qu&eacute;bec &agrave; Montr&eacute;al, Management Science Pavilion", type: "University", address: "315 Rue Sainte-Catherine Est", site: "https://carte.uqam.ca/pavillon-r"},
-	{name: "Fairfield Inn by Montreal Marriott Downtown", type: "Hotel", address: "1199 Rue Berri", site: "https://www.marriott.com/hotels/travel/yulfd-fairfield-montreal-downtown/"}];
+	{name: "Fairfield Inn by Montreal Marriott Downtown", type: "Hotel", address: "1199 Rue Berri", site: "https://www.marriott.com/hotels/travel/yulfd-fairfield-montreal-downtown/"},
+	{name: "Universit&eacute; du Qu&eacute;bec &agrave; Montr&eacute;al, Sports Centre", type: "Sports", address: "1212 Rue Sanguinet", site: "https://carte.uqam.ca/pavillon-cs"}];
 	
 	if(spots.length == 1){
 		document.getElementById("hotspot-js").innerHTML += "<span style=\"font-size: 14pt\"><b>"+spots.length+ " hotspot is near this station.</b></span><br/><br/>";
