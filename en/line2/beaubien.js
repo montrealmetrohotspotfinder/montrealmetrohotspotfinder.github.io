bbba8f996f60c2138164f9a88ac89591911e5b7e
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-05-05 17:41:47 -0400

    Update beaubien.js

--- en/line2/beaubien.js
+++ en/line2/beaubien.js
@@ -4,7 +4,9 @@ function init()
 	{name: "&Eacute;glise Notre-Dame-de-la-D&eacute;fense", type: "Church", address: "6800 Avenue Henri-Julien", site: "https://diocesemontreal.org/en/lets-connect/churches-parishes/notre-dame-de-la-defense-italiens"},
 	{name: "&Eacute;glise Saint-&Eacute;douard", type: "Church", address: "6500 Rue de Saint-Vallier", site: "#"},
 	{name: "&Eacute;cole La Mennais", type: "School", address: "6521 Rue Saint-Denis", site: "https://la-mennais.csdm.ca/"},
-	{name: "Chambre de la Jeunesse", type: "Courthouse", address: "410 Rue de Bellechasse", site: "https://www.justice.gouv.qc.ca/en/judicial-system/courts-and-tribunals-of-quebec/court-of-quebec/youth-division"}];
+	{name: "Chambre de la Jeunesse", type: "Courthouse", address: "410 Rue de Bellechasse", site: "https://www.justice.gouv.qc.ca/en/judicial-system/courts-and-tribunals-of-quebec/court-of-quebec/youth-division"},
+	{name: "Th&eacute;&acirc;tre Plaza", type: "Theatre", address: "6505 Rue Saint-Hubert", site: "https://www.theatreplaza.ca/"},
+	{name: "Beaubien Bagel", type: "Restaurant", address: "828 Rue Beaubien Est", site: "#"}];
 	
 	if(spots.length == 1){
 		document.getElementById("hotspot-js").innerHTML += "<span style=\"font-size: 14pt\"><b>"+spots.length+ " hotspot is near this station.</b></span><br/><br/>";
