c24191e98aaa50423b25b0cd31413fc2734eb6ba
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-05-09 16:33:59 -0400

    Update recherche.js

--- fr/recherche.js
+++ fr/recherche.js
@@ -468,17 +468,17 @@ function init(){
 	"https://montreal.ca/lieux/place-emmett-johns", "http://www.appartementsfaro.ca/", "https://editionsmd.com/" ];
 	
 	Beaudrynames = [ "Le Village", "Maison Radio-Canada", "Cabaret Mado", "L&#39;Olympia", "Parc de l&#39;Espoir", "Parc Serge-Garant", "Le National", "Le Pass&eacute; Compos&eacute;", "Parc Fran&ccedil;ois-Martineau", "Parc Hector-Charland",
-	"Centre St-Pierre", "Parc des Joyeux-Vikings", "Parc Jean-Charbonneau", "Ar&eacute;na Camillien-Houde" ];
+	"Centre St-Pierre", "Parc des Joyeux-Vikings", "Parc Jean-Charbonneau", "Ar&eacute;na Camillien-Houde", "Parc Miville-Couture", "Le Rubic", "&Eacute;glise Saint-Pierre-Ap&ocirc;tre" ];
 	
-	Beaudrytypes = [ "District", "Bureau", "Restaurant", "Th&eacute;&acirc;tre", "Parc", "Parc", "Th&eacute;&acirc;tre", "Restaurant", "Parc", "Parc", "&Eacute;cole", "Parc", "Parc", "Sports" ];
+	Beaudrytypes = [ "District", "Bureau", "Restaurant", "Th&eacute;&acirc;tre", "Parc", "Parc", "Th&eacute;&acirc;tre", "Restaurant", "Parc", "Parc", "&Eacute;cole", "Parc", "Parc", "Sports", "Parc", "R&eacute;sidence", "&Eacute;glise" ];
 	
 	Beaudryaddresses = [ "", "1400 Boulevard Ren&eacute;-L&eacute;vesque Est", "1115 Rue Sainte-Catherine Est", "1004 Rue Sainte-Catherine Est", "1336 Rue Sainte-Catherine Est", "", "1220 Rue Sainte-Catherine Est", "1310 Boulevard de Maisonneuve Est",
-	"", "1662 Rue de la Visitation", "1212 Rue Panet", "1763 Rue Beaudry", "", "1696 Rue Montcalm" ];
+	"", "1662 Rue de la Visitation", "1212 Rue Panet", "1763 Rue Beaudry", "", "1696 Rue Montcalm", "1215 Rue Atateken", "1117 Boulevard Ren&eacute;-L&eacute;vesque Est", "1201 Rue de la Visitation" ];
 	
 	Beaudrysites = [ "https://www.mtl.org/fr/quoi-faire/patrimoine-et-architecture/le-village-montreal", "http://archives.radio-canada.ca/societe/histoire/topics/1069-5953/", 
 	"http://www.mado.qc.ca/", "https://www.olympiamontreal.com/", "https://montreal.ca/lieux/parc-de-lespoir", "https://www.lavitrine.com/place/Parc_Serge_Garant", "http://www.latulipe.ca/", "https://www.bistropassecompose.com/",
 	"https://montreal.ca/lieux/parc-francois-martineau", "https://montreal.ca/lieux/parc-hector-charland", "https://www.centrestpierre.org/", "https://montreal.ca/lieux/parc-des-joyeux-vikings", "https://montreal.ca/lieux/parc-jean-charbonneau",
-	"https://montreal.ca/lieux/arena-camillien-houde" ];
+	"https://montreal.ca/lieux/arena-camillien-houde", "https://montreal.ca/lieux/parc-miville-couture", "https://rubic.ca/", "http://www.saintpierreapotre.ca/" ];
 	
 	SaintLaurentnames = [ "Mus&eacute;e Juste Pour Rire", "Club Soda", "Best Western Plus H&ocirc;tel Montr&eacute;al", "Monument-National", "Laurent & Clark", "Appartements Le Myriade", "MTelus", "Place de la Paix", "H&ocirc;tel Z&eacute;ro1",
 		 "H&ocirc;tel Faubourg Montr&eacute;al", "H&ocirc;tel Chrome Montr&eacute;al Centre-Ville", "Candlewood Suites Montr&eacute;al Centre-Ville",
