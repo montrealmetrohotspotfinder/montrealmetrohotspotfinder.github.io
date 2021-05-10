422868008c33e0a9888f63f2e13e0f15c4bd11d0
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-05-10 04:42:05 -0400

    Update d-iberville.js

--- en/line5/d-iberville.js
+++ en/line5/d-iberville.js
@@ -3,9 +3,10 @@ function init()
 	spots = [{name: "P&acirc;tisserie La Conca D&#39;Oro", type: "Shopping", address: "2550 Rue Jean-Talon Est", site: "https://www.laconcadoro.net/"},
 	{name: "Parc Gabriel-Sagard", type: "Park", address: "7069 Rue Sagard", site: "https://montreal.ca/en/places/parc-gabriel-sagard"},
 	{name: "&Eacute;cole Saint-Barth&eacute;lemy", type: "School", address: "7081 Avenue des &Eacute;rables", site: "https://st-barthelemy.csdm.ca/"},
-	{name: "&Eacute;glise Biblique Pierre Angulaire", type: "Church", address: "7137 Avenue des &Eacute;rables", site: "#"},
+	{name: "&Eacute;glise Biblique Pierre Angulaire", type: "Church", address: "2200 Rue Jean-Talon Est", site: "#"},
 	{name: "Parc Molson", type: "Park", address: "2473 Rue Beaubien Est", site: "https://montreal.ca/en/places/parc-molson"},
-	{name: "Paroisse Saint-Mathieu", type: "Church", address: "2600 Rue Jean-Talon Est", site: "https://www.diocesemontreal.org/en/lets-connect/churches-parishes/saint-mathieu"}];
+	{name: "Paroisse Saint-Mathieu", type: "Church", address: "2600 Rue Jean-Talon Est", site: "https://www.diocesemontreal.org/en/lets-connect/churches-parishes/saint-mathieu"},
+	{name: "Paroisse Saint-Barth&eacute;lemy", type: "Church", address: "7137 Avenue des &Eacute;rables", site: "https://diocesemontreal.org/en/lets-connect/churches-parishes/saint-barthelemy"}];
 	
 	if(spots.length == 1){
 		document.getElementById("hotspot-js").innerHTML += "<span style=\"font-size: 14pt\"><b>"+spots.length+ " hotspot is near this station.</b></span><br/><br/>";
