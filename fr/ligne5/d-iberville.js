701a5143de15bec62cc41be5943b17d9f3864b96
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-05-10 04:48:41 -0400

    Update d-iberville.js

--- fr/ligne5/d-iberville.js
+++ fr/ligne5/d-iberville.js
@@ -1,15 +1,15 @@
 function init()
 {
 	hnames = [ "P&acirc;tisserie La Conca D&#39;Oro", "Parc Gabriel-Sagard", "&Eacute;cole Saint-Barth&eacute;lemy", "&Eacute;glise Biblique Pierre Angulaire",
-	"Parc Molson", "Paroisse Saint-Mathieu" ];
+	"Parc Molson", "Paroisse Saint-Mathieu", "Paroisse Saint-Barth&eacute;lemy" ];
 	
-	htypes = [ "Magasinage", "Parc", "&Eacute;cole", "&Eacute;glise", "Parc", "&Eacute;glise" ];
+	htypes = [ "Magasinage", "Parc", "&Eacute;cole", "&Eacute;glise", "Parc", "&Eacute;glise", "&Eacute;glise" ];
 	
-	haddresses = [ "2550 Rue Jean-Talon Est", "7069 Rue Sagard", "7081 Avenue des &Eacute;rables", "7137 Avenue des &Eacute;rables", "2473 Rue Beaubien Est",
-	"2600 Rue Jean-Talon Est" ];
+	haddresses = [ "2550 Rue Jean-Talon Est", "7069 Rue Sagard", "7081 Avenue des &Eacute;rables", "2200 Rue Jean-Talon Est", "2473 Rue Beaubien Est",
+	"2600 Rue Jean-Talon Est", "7137 Avenue des &Eacute;rables" ];
 	
 	hsites = [ "https://alexiaianniciello1.wixsite.com/fvlaconcadoro", "https://montreal.ca/lieux/parc-gabriel-sagard", "https://st-barthelemy.csdm.ca/", "#",
-	"https://montreal.ca/lieux/parc-molson", "https://www.diocesemontreal.org/fr/vie-communautaire/eglises-paroisses/saint-mathieu" ];
+	"https://montreal.ca/lieux/parc-molson", "https://www.diocesemontreal.org/fr/vie-communautaire/eglises-paroisses/saint-mathieu", "https://diocesemontreal.org/fr/vie-communautaire/eglises-paroisses/saint-barthelemy" ];
 	
 	if(hnames.length == 1){
 		document.getElementById("hotspot-js").innerHTML += "<span style=\"font-size: 14pt\"><em lang=\"fr\"><b>"+hnames.length+ " point d&#39;int&eacute;r&ecirc;t est &agrave; proximit&eacute; de cette station.</b></em></span><br/><br/>";
