c99ef00d65e0bdd7073df079b619b9e14de7132f
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-05-10 04:50:02 -0400

    Update recherche.js

--- fr/recherche.js
+++ fr/recherche.js
@@ -233,15 +233,15 @@ function init(){
 	"https://www.centreboulevard.com/fr/", "https://www.residencelesaintmichel.ca/" ];
 	
 	Ibervillenames = [ "P&acirc;tisserie La Conca D&#39;Oro", "Parc Gabriel-Sagard", "&Eacute;cole Saint-Barth&eacute;lemy", "&Eacute;glise Biblique Pierre Angulaire",
-	"Parc Molson", "Paroisse Saint-Mathieu" ];
+	"Parc Molson", "Paroisse Saint-Mathieu", "Paroisse Saint-Barth&eacute;lemy" ];
 	
-	Ibervilletypes = [ "Magasinage", "Parc", "&Eacute;cole", "&Eacute;glise", "Parc", "&Eacute;glise" ];
+	Ibervilletypes = [ "Magasinage", "Parc", "&Eacute;cole", "&Eacute;glise", "Parc", "&Eacute;glise", "&Eacute;glise" ];
 	
-	Ibervilleaddresses = [ "2550 Rue Jean-Talon Est", "7069 Rue Sagard", "7081 Avenue des &Eacute;rables", "7137 Avenue des &Eacute;rables", "2473 Rue Beaubien Est",
-	"2600 Rue Jean-Talon Est" ];
+	Ibervilleaddresses = [ "2550 Rue Jean-Talon Est", "7069 Rue Sagard", "7081 Avenue des &Eacute;rables", "2200 Rue Jean-Talon Est", "2473 Rue Beaubien Est",
+	"2600 Rue Jean-Talon Est", "7137 Avenue des &Eacute;rables" ];
 	
 	Ibervillesites = [ "https://alexiaianniciello1.wixsite.com/fvlaconcadoro", "https://montreal.ca/lieux/parc-gabriel-sagard", "https://st-barthelemy.csdm.ca/", "#",
-	"https://montreal.ca/lieux/parc-molson", "https://www.diocesemontreal.org/fr/vie-communautaire/eglises-paroisses/saint-mathieu" ];
+	"https://montreal.ca/lieux/parc-molson", "https://www.diocesemontreal.org/fr/vie-communautaire/eglises-paroisses/saint-mathieu", "https://diocesemontreal.org/fr/vie-communautaire/eglises-paroisses/saint-barthelemy" ];
 	
 	Fabrenames = [ "&Eacute;glise Notre-Dame-de-la-Consolata", "H&ocirc;pital Jean-Talon", "Parc de Turin", "&Eacute;cole Saint-Gabriel-Lalemant", "&Eacute;cole Pierre Elliott Trudeau" ];
 	
