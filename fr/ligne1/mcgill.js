7fc0bce7db47045a9022d87e4ea8da493504fe05
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-05-20 17:24:30 -0400

    Update mcgill.js

--- fr/ligne1/mcgill.js
+++ fr/ligne1/mcgill.js
@@ -53,12 +53,14 @@ function init()
 		"Universit&eacute; McGill, &Eacute;cole des Sciences Informatiques",
 		"Universit&eacute; McGill, D&eacute;partement de Chimie",
 		"Universit&eacute; McGill, Facult&eacute; d&#39;Ing&eacute;nierie",
-		"Le Coll&egrave;ge Presbyt&eacute;rien" ];
+		"Le Coll&egrave;ge Presbyt&eacute;rien",
+		"Fonds d&#39;Assurance Responsabilit&eacute; Professionnelle de l&#39;Ordre des Pharmaciens du Qu&eacute;bec",
+		"RSA Group, bureaux montr&eacute;alais" ];
 	
 	htypes = [ "Universit&eacute;", "Centre Commercial", "Centre Commercial", "Centre Commercial", "H&ocirc;tel", "Mus&eacute;e", "Mus&eacute;e", "Mus&eacute;e", "Place de la Ville", "Place de la Ville", "&Eacute;glise", 
 	"Bureau", "H&ocirc;pital", "Sports", "Centre Commercial", "Cin&eacute;ma", "Bureau", "Centre Commercial", "&Eacute;glise", "Magasinage", "Bureau", "Bureau", "Bureau", "Bureau", "Bureau", "Bureau", "Bureau", "H&ocirc;tel", "Agence", "H&ocirc;tel",
 	"Agence", "&Eacute;cole", "Site Historique", "Bureau", "Bureau", "Coll&egrave;ge", "Agence", "Bureau", "R&eacute;sidence", "Universit&eacute;", "Universit&eacute;", "Agence", "&Eacute;cole", "Agence", "H&ocirc;tel", "&Eacute;cole de M&eacute;tiers",
-	"Bureau", "Universit&eacute;", "Universit&eacute;", "Universit&eacute;", "Universit&eacute;", "Universit&eacute;", "Universit&eacute;", "Coll&egrave;ge" ];
+	"Bureau", "Universit&eacute;", "Universit&eacute;", "Universit&eacute;", "Universit&eacute;", "Universit&eacute;", "Universit&eacute;", "Coll&egrave;ge", "Agence", "Bureau" ];
 	
 	haddresses = [ "845 Rue Sherbrooke Ouest", "705 Rue Sainte-Catherine Ouest", "1500 Avenue McGill College", "1 Place Ville Marie", "900 Boulevard Ren&eacute;-L&eacute;vesque Ouest",
 	"705 Rue Sainte-Catherine Ouest", "690 Rue Sherbrooke Ouest", "859 Rue Sherbrooke Ouest", "", "2903 Rue Peel", "1085 Rue de la Cath&eacute;drale", "1155 Rue Metcalfe", 
@@ -68,7 +70,8 @@ function init()
 	"1250 Boulevard Robert-Bourassa", "500 Boulevard Ren&eacute;-L&eacute;vesque Ouest", "1240 Square Phillips", "1200 Avenue McGill College", "550 Rue Sherbrooke Ouest", "15a Rue Sherbrooke Ouest",
 	"2001 Boulevard Robert-Bourassa", "2001 Boulevard Robert-Bourassa", "1255 Boulevard Robert-Bourassa", "500 Boulevard Ren&eacute;-L&eacute;vesque Ouest", "1981 Avenue McGill College",
 	"1188 Avenue Union", "2001 Avenue McGill College", "688 Rue Sherbrooke Ouest", "1801 Avenue McGill College", "3449 Rue University", "500 Boulevard Ren&eacute;-L&eacute;vesque Ouest", "1193 Place Phillips", "505 Boulevard Ren&eacute;-L&eacute;vesque Ouest",
-	"1981 Avenue McGill College", "680 Rue Sherbrooke Ouest", "853 Rue Sherbrooke Ouest", "805 Rue Sherbrooke Ouest", "3480 Rue University", "801 Rue Sherbrooke Ouest", "817 Rue Sherbrooke Ouest", "3495 Boulevard Robert-Bourassa" ];
+	"1981 Avenue McGill College", "680 Rue Sherbrooke Ouest", "853 Rue Sherbrooke Ouest", "805 Rue Sherbrooke Ouest", "3480 Rue University", "801 Rue Sherbrooke Ouest", "817 Rue Sherbrooke Ouest", "3495 Boulevard Robert-Bourassa",
+	"2020 Boulevard Robert-Bourassa", "2000 Avenue McGill College" ];
 	
 	hsites = [ "https://www.mcgill.ca/fr", "https://www.centreeatondemontreal.com/fr", "https://www.placemontrealtrust.com/fr", "https://placevillemarie.com/fr/accueil", "https://www.fairmont.fr/queen-elizabeth-montreal/",
 	"https://www.grevin-montreal.com/", "https://www.musee-mccord.qc.ca/fr", "https://www.mcgill.ca/redpath/fr", "https://montreal.ca/lieux/square-phillips", 
@@ -82,7 +85,7 @@ function init()
 	"https://gwlraleasing.com/building.php?building=4209522867010994813&lang=fr_CA", "https://www.montrealcollege.ca/fr/", "http://www.habitation.gouv.qc.ca/", "https://gaa.qc.ca/", "https://tomcondos.com/fr/", "https://www.mcgill.ca/dentistry/fr",
 	"https://www.mcgill.ca/continuingstudies/fr", "http://www.oiq.qc.ca/Pages/accueil.aspx", "https://face-english.cssdm.gouv.qc.ca/", "https://comite.deontologie.gouv.qc.ca/accueil.html", "http://www.squarephillips.com/fr/accueil.html", "https://eequebec.com/",
 	"https://www.richter.ca/fr/", "https://www.mcgill.ca/maxbellschool/fr", "https://www.mcgill.ca/science/fr", "https://www.mcgill.ca/geography/fr", "https://www.cs.mcgill.ca/", "https://www.mcgill.ca/chemistry/fr", "https://www.mcgill.ca/engineering/",
-	"https://www.presbyteriancollege.ca/" ];
+	"https://www.presbyteriancollege.ca/", "https://www.farpopq.com/", "https://www.rsagroup.ca/fr" ];
 	
 	if(hnames.length == 1){
 		document.getElementById("hotspot-js").innerHTML += "<span style=\"font-size: 14pt\"><em lang=\"fr\"><b>"+hnames.length+ " point d&#39;int&eacute;r&ecirc;t est &agrave; proximit&eacute; de cette station.</b></em></span><br/><br/>";
