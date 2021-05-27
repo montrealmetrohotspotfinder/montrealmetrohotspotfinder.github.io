2ca4ac54152dcd3185729ea96f6b7ed7555f655f
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-05-27 15:26:08 -0400

    Update edouard-montpetit.js

--- fr/ligne5/edouard-montpetit.js
+++ fr/ligne5/edouard-montpetit.js
@@ -1,12 +1,13 @@
 function init()
 {
-	hnames = [ "CEPSUM", "&Eacute;cole de Musique Vincent-d&#39;Indy", "&Eacute;cole Saint-Germain-d&#39;Outremont", "Paroisse Saint-Germain-d&#39;Outremont", "Universit&eacute; de Montr&eacute;al, Facult&eacute; de Musique" ];
+	hnames = [ "CEPSUM", "&Eacute;cole de Musique Vincent-d&#39;Indy", "&Eacute;cole Saint-Germain-d&#39;Outremont", "Paroisse Saint-Germain-d&#39;Outremont", "Universit&eacute; de Montr&eacute;al, Facult&eacute; de Musique",
+	"Universit&eacute; de Montr&eacute;al, Facult&eacute; des Sciences Infirmi&egrave;res" ];
 	
-	htypes = [ "Sports", "Coll&egrave;ge", "&Eacute;cole", "&Eacute;glise", "Universit&eacute;" ];
+	htypes = [ "Sports", "Coll&egrave;ge", "&Eacute;cole", "&Eacute;glise", "Universit&eacute;", "Universit&eacute;" ];
 	
-	haddresses = [ "2100 Boulevard &Eacute;douard-Montpetit", "628 Chemin de la C&ocirc;te-Sainte-Catherine", "46 Avenue Vincent-d&#39;Indy", "28 Avenue Vincent-d&#39;Indy", "200 Avenue Vincent-d&#39;Indy" ];
+	haddresses = [ "2100 Boulevard &Eacute;douard-Montpetit", "628 Chemin de la C&ocirc;te-Sainte-Catherine", "46 Avenue Vincent-d&#39;Indy", "28 Avenue Vincent-d&#39;Indy", "200 Avenue Vincent-d&#39;Indy", "2375 Chemin de la C&ocirc;te-Sainte-Catherine" ];
 	
-	hsites = [ "https://www.cepsum.umontreal.ca/", "http://www.emvi.qc.ca/", "https://www.csmb.qc.ca/fr-CA/enseignement/primaire/ecoles/liste/saint-germain-doutremont.aspx", "https://paroisse-saint-germain.org/", "https://musique.umontreal.ca/accueil/" ];
+	hsites = [ "https://www.cepsum.umontreal.ca/", "http://www.emvi.qc.ca/", "https://www.csmb.qc.ca/fr-CA/enseignement/primaire/ecoles/liste/saint-germain-doutremont.aspx", "https://paroisse-saint-germain.org/", "https://musique.umontreal.ca/accueil/", "https://fsi.umontreal.ca/accueil/" ];
 	
 	if(hnames.length == 1){
 		document.getElementById("hotspot-js").innerHTML += "<span style=\"font-size: 14pt\"><em lang=\"fr\"><b>"+hnames.length+ " point d&#39;int&eacute;r&ecirc;t est &agrave; proximit&eacute; de cette station.</b></em></span><br/><br/>";
