6015ec41917bee53ecfa7093deca3cc4e59d0f54
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-05-30 19:39:51 -0400

    Update longueuil-universite-de-sherbrooke.js

--- en/line4/longueuil-universite-de-sherbrooke.js
+++ en/line4/longueuil-universite-de-sherbrooke.js
@@ -14,7 +14,8 @@ function init()
 	{name: "Le D&#39;Assigny", type: "Residence", address: "110 Rue de la Barre", site: "https://groupemercille.com/residential/3-12#le-dassigny"},
 	{name: "Revenu Qu&eacute;bec, Longueuil offices", type: "Agency", address: "825 Rue Saint-Laurent Ouest", site: "https://www.revenuquebec.ca/en/"},
 	{name: "Tribunal Administratif du Logement du Qu&eacute;bec, Longueuil offices", type: "Agency", address: "201 Place Charles-Lemoyne", site: "https://www.tal.gouv.qc.ca/en"},
-	{name: "Les Habitats Lafayette", type: "Residence", address: "100 Boulevard La Fayette", site: "http://www.residences6etoiles.com/en/lafayette.html"}];
+	{name: "Les Habitats Lafayette", type: "Residence", address: "100 Boulevard La Fayette", site: "http://www.residences6etoiles.com/en/lafayette.html"},
+	{name: "SSQ Insurance, Longueuil offices", type: "Office", address: "1225 Rue Saint-Charles Ouest", site: "https://ssq.ca/en"}];
 		
 	if(spots.length == 1){
 		document.getElementById("hotspot-js").innerHTML += "<span style=\"font-size: 14pt\"><b>"+spots.length+ " hotspot is near this station.</b></span><br/><br/>";
