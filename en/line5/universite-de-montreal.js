bc8afa1f075d106c4de6561fab8e08cd0db0470a
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-05-29 21:55:41 -0400

    Update universite-de-montreal.js

--- en/line5/universite-de-montreal.js
+++ en/line5/universite-de-montreal.js
@@ -5,7 +5,9 @@ function init()
 	{name: "CHU Sainte-Justine", type: "Hospital", address: "3175 Chemin de la C&ocirc;te-Sainte-Catherine", site: "https://www.chusj.org/Home"},
 	{name: "HEC Montr&eacute;al", type: "School", address: "3000 Chemin de la C&ocirc;te-Sainte-Catherine", site: "https://www.hec.ca/en/"},
 	{name: "Universit&eacute; de Montr&eacute;al, Faculty of Environmental Design", type: "University", address: "2940 Chemin de la C&ocirc;te-Sainte-Catherine", site: "https://amenagement.umontreal.ca/accueil/"},
-	{name: "Universit&eacute; de Montr&eacute;al, Faculty of Medicine", type: "University", address: "2900 Boulevard &Eacute;douard-Montpetit", site: "https://medecine.umontreal.ca/medical-school/"}];
+	{name: "Universit&eacute; de Montr&eacute;al, Faculty of Medicine", type: "University", address: "2900 Boulevard &Eacute;douard-Montpetit", site: "https://medecine.umontreal.ca/medical-school/"},
+	{name: "Universit&eacute; de Montr&eacute;al, Faculty of Pharmacy", type: "University", address: "2940 Chemin de Polytechnique", site: "https://pharm.umontreal.ca/english/home/"},
+	{name: "Polytechnique Montr&eacute;al", type: "University", address: "2500 Chemin de Polytechnique", site: "https://www.polymtl.ca/en/"}];
 	
 	if(spots.length == 1){
 		document.getElementById("hotspot-js").innerHTML += "<span style=\"font-size: 14pt\"><b>"+spots.length+ " hotspot is near this station.</b></span><br/><br/>";
