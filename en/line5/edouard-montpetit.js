f7de28fd0b06cf8d5bca7003ca3ab109e7e2294e
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-05-29 21:35:03 -0400

    Update edouard-montpetit.js

--- en/line5/edouard-montpetit.js
+++ en/line5/edouard-montpetit.js
@@ -4,8 +4,8 @@ function init()
 	{name: "&Eacute;cole de Musique Vincent-d&#39;Indy", type: "College", address: "628 Chemin de la C&ocirc;te-Sainte-Catherine", site: "http://www.emvi.qc.ca/"},
 	{name: "&Eacute;cole Saint-Germain-d&#39;Outremont", type: "School", address: "46 Avenue Vincent-d&#39;Indy", site: "https://www.csmb.qc.ca/fr-CA/enseignement/primaire/ecoles/liste/saint-germain-doutremont.aspx"},
 	{name: "Paroisse Saint-Germain-d&#39;Outremont", type: "Church", address: "28 Avenue Vincent-d&#39;Indy", site: "https://paroisse-saint-germain.org/"},
-	{name: "Universit&eacute; de Montr&eacute;al, Music Faculty", type: "University", address: "200 Avenue Vincent-d&#39;Indy", site: "https://musique.umontreal.ca/en/home/"},
-	{name: "Universit&eacute; de Montr&eacute;al, Nursing Sciences Faculty", type: "University", address: "2375 Chemin de la C&ocirc;te-Sainte-Catherine", site: "https://fsi.umontreal.ca/accueil/"},
+	{name: "Universit&eacute; de Montr&eacute;al, Faculty of Music", type: "University", address: "200 Avenue Vincent-d&#39;Indy", site: "https://musique.umontreal.ca/en/home/"},
+	{name: "Universit&eacute; de Montr&eacute;al, Faculty of Nursing Sciences", type: "University", address: "2375 Chemin de la C&ocirc;te-Sainte-Catherine", site: "https://fsi.umontreal.ca/accueil/"},
 	{name: "Cath&eacute;drale Orthodoxe Grecque Saint-Georges", type: "Church", address: "2455 Chemin de la C&ocirc;te-Sainte-Catherine", site: "https://hcgm.org/churches/st-george-cathedral/"},
 	{name: "&Eacute;glise Saint-Albert-le-Grand", type: "Church", address: "2715 Chemin de la C&ocirc;te-Sainte-Catherine", site: "http://www.st-albert.org/"}];
 	
