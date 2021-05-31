8dabd07eadb4b2b949c6d7bb1d592b0a49072743
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-05-31 12:18:14 -0400

    Update search.js

--- en/search.js
+++ en/search.js
@@ -458,7 +458,8 @@ function init(){
 	{name: "McGill University, Department of Psychology", type: "University", address: "2001 Avenue McGill College", site: "https://www.mcgill.ca/psychology/"},
 	{name: "Fiera Capital", type: "Office", address: "1981 Avenue McGill College", site: "https://www.fieracapital.com/en"},
 	{name: "McGill University, Faculty of Arts", type: "University", address: "853 Rue Sherbrooke Ouest", site: "https://www.mcgill.ca/arts/"},
-	{name: "McGill University, School of Urban Planning", type: "University", address: "815 Rue Sherbrooke Ouest", site: "https://www.mcgill.ca/urbanplanning/"}];
+	{name: "McGill University, School of Urban Planning", type: "University", address: "815 Rue Sherbrooke Ouest", site: "https://www.mcgill.ca/urbanplanning/"},
+	{name: "Valero, Montreal offices", type: "Office", address: "1801 Avenue McGill College", site: "https://www.valero.com/"}];
 	
 	Peel = [{name: "Les Cours Mont-Royal", type: "Mall", address: "1455 Rue Peel", site: "https://lcmr.ca/"},
 	{name: "Carrefour Industrielle Alliance", type: "Mall", address: "977 Rue Sainte-Catherine Ouest", site: "http://www.carrefourindustriellealliance.com/en"},
