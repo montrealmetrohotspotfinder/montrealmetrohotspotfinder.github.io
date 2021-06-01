79c5105d2a8ca02f1c3853e8ac64ce9f3cf02a80
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-06-01 15:40:17 -0400

    Update search.js

--- en/search.js
+++ en/search.js
@@ -40,7 +40,8 @@ function init(){
 	{name: "Universit&eacute; du Qu&eacute;bec &agrave; Montr&eacute;al, Sports Centre", type: "Sports", address: "1212 Rue Sanguinet", site: "https://carte.uqam.ca/pavillon-cs"},
 	{name: "Biblioth&egrave;que Saint-Sulpice", type: "Library", address: "1700 Rue Saint-Denis", site: "https://banq-saint-sulpice.business.site/"},
 	{name: "Boxotel", type: "Hotel", address: "175 Rue Ontario Est", site: "https://www.boxotel.com/"},
-	{name: "H&eacute;ritage Victorien", type: "Hotel", address: "305 Rue Ontario Est", site: "http://www.montrealbedandbreakfast.ca/index.html"}];
+	{name: "H&eacute;ritage Victorien", type: "Hotel", address: "305 Rue Ontario Est", site: "http://www.montrealbedandbreakfast.ca/index.html"},
+	{name: "Cath&eacute;drale Saint-Jacques", type: "Church", address: "1455 Rue Saint-Denis", site: "#"}];
 	
 	JeanTalon = [{name: "Tour Jean-Talon", type: "Office", address: "600 Rue Jean-Talon Est", site: "https://montrealundergroundcity.com/tour/tour-jean-talon/"},
 	{name: "March&eacute; Jean-Talon", type: "Market", address: "7070 Avenue Henri-Julien", site: "https://www.marchespublics-mtl.com/en/marches/jean-talon-market/"},
