9bf11c9934dd6318c0fc26ce52e9022f502ed0b5
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-06-01 14:14:12 -0400

    Update search.js

--- en/search.js
+++ en/search.js
@@ -355,7 +355,8 @@ function init(){
 	{name: "H&ocirc;tel Faubourg Montr&eacute;al", type: "Hotel", address: "155 Boulevard Ren&eacute;-L&eacute;vesque Est", site: "https://www.hotelfaubourgmontreal.com/"},
 	{name: "H&ocirc;tel Chrome Montr&eacute;al Centre-Ville", type: "Hotel", address: "215 Boulevard Ren&eacute;-L&eacute;vesque Est", site: "https://www.hotelchrome.com/"},
 	{name: "Candlewood Suites Montr&eacute;al Centre-Ville", type: "Hotel", address: "191 Boulevard Ren&eacute;-L&eacute;vesque Est", site: "https://www.ihg.com/candlewood/hotels/us/en/montreal/yullb/hoteldetail?cm_mmc=GoogleMaps-_-CW-_-CA-_-YULLB"},
-	{name: "Universit&eacute; du Qu&eacute;bec &agrave; Montr&eacute;al, Sainte-Catherine Pavilion", type: "University", address: "209 Rue Sainte-Catherine Est", site: "http://carte.uqam.ca/pavillon-v"}];
+	{name: "Universit&eacute; du Qu&eacute;bec &agrave; Montr&eacute;al, Sainte-Catherine Pavilion", type: "University", address: "209 Rue Sainte-Catherine Est", site: "http://carte.uqam.ca/pavillon-v"},
+	{name: "H&ocirc;tel 10", type: "Hotel", address: "10 Rue Sherbrooke Ouest", site: "https://www.hotel10montreal.com/"}];
 	
 	PlaceDesArts = [{name: "Place des Arts", type: "Theatre", address: "175 Rue Sainte-Catherine Ouest", site: "https://placedesarts.com/en"},
 	{name: "Quartier des Spectacles / Place des Festivals", type: "District", address: "", site: "https://www.quartierdesspectacles.com/en/"},
