9778a59d89a67bdf96a9a483a7c8029fb629e069
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-06-04 01:42:52 -0400

    Update de-l-eglise.js

--- fr/ligne1/de-l-eglise.js
+++ fr/ligne1/de-l-eglise.js
@@ -1,12 +1,12 @@
 function init()
 {
 	hnames = [ "Promenade Wellington", "Auditorium de Verdun", "Plage de Verdun", "Parc Arthur-Therrien", "Stade Ronald-Pich&eacute;", "H&ocirc;pital de Verdun", "Parc J.-Albert-Gari&eacute;py",
-	"&Eacute;glise Notre-Dame-des-Sept-Douleurs de Montr&eacute;al" ];
+	"&Eacute;glise Notre-Dame-des-Sept-Douleurs de Montr&eacute;al", "&Eacute;glise de l&#39;&Eacute;piphanie", "&Eacute;cole Notre-Dame-des-Sept-Douleurs", "Onward Gospel Church", "Parc Joannette" ];
 	
-	htypes = [ "Magasinage", "Sports", "Plage", "Parc", "Sports", "H&ocirc;pital", "Parc", "&Eacute;glise" ];
+	htypes = [ "Magasinage", "Sports", "Plage", "Parc", "Sports", "H&ocirc;pital", "Parc", "&Eacute;glise", "&Eacute;glise", "&Eacute;cole", "&Eacute;glise", "Parc" ];
 	
 	haddresses = [ "266 Rue Hickson", "4110 Boulevard LaSalle", "4110 Boulevard LaSalle", "3750 Boulevard Ga&eacute;tan-Laberge", "3750 Boulevard Ga&eacute;tan-Laberge",
-	"4000 Boulevard LaSalle", "10 All&eacute;e des Brises-du-Fleuve", "4155 Rue Wellington" ];
+	"4000 Boulevard LaSalle", "10 All&eacute;e des Brises-du-Fleuve", "4155 Rue Wellington", "4322 Rue Wellington", "320 Rue de l&#39;&Eacute;glise", "165 Rue Galt", "292 Rue Hickson" ];
 	
 	hsites = [ "http://www.promenadewellington.com/fr", 
 	"https://montreal.ca/lieux/auditorium-de-verdun", 
@@ -15,7 +15,11 @@ function init()
 	"#",
 	"https://www.ciusss-centresudmtl.gouv.qc.ca/etablissement/hopital-de-verdun",
 	"https://montreal.ca/lieux/parc-j-albert-gariepy",
-	"http://nddt.org/" ];
+	"http://nddt.org/",
+	"https://epiphanyverdun.com/",
+	"https://nddsd.ecoleverdun.com/",
+	"http://www.onwardgospelchurch.com/",
+	"https://montreal.ca/lieux/parc-joannette" ];
 	
 	if(hnames.length == 1){
 		document.getElementById("hotspot-js").innerHTML += "<span style=\"font-size: 14pt\"><em lang=\"fr\"><b>"+hnames.length+ " point d&#39;int&eacute;r&ecirc;t est &agrave; proximit&eacute; de cette station.</b></em></span><br/><br/>";
