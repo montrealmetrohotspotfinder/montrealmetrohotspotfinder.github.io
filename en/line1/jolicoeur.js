0b744647d3bd2b3a1681d10c835136f4d8b72768
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-06-04 15:32:26 -0400

    Update jolicoeur.js

--- en/line1/jolicoeur.js
+++ en/line1/jolicoeur.js
@@ -1,6 +1,9 @@
 function init()
 {
-	spots = [{name: "Parc de la V&eacute;rendrye", type: "Park", address: "5900 Rue Drake", site: "https://montreal.ca/en/places/parc-de-la-verendrye"}];
+	spots = [{name: "Parc de la V&eacute;rendrye", type: "Park", address: "5900 Rue Drake", site: "https://montreal.ca/en/places/parc-de-la-verendrye"},
+		{name: "&Eacute;glise Notre-Dame-Porte-de-l&#39;Aurore", type: "Church", address: "1465 Rue de S&egrave;ve", site: "https://diocesemontreal.org/en/lets-connect/churches-parishes/notre-dame-porte-de-laurore-lituaniens"},
+		{name: "&Eacute;glise Coeur-Immacul&eacute;-de-Marie", type: "Church", address: "6300 Rue Laurendeau", site: "https://www.diocesemontreal.org/en/faith-community/church/coeur-immacule-de-marie"},
+		{name: "&Eacute;cole Coeur-Immacul&eacute;-de-Marie", type: "School", address: "1845 Boulevard Desmarchais", site: "https://coeur-immacule-de-marie.cssdm.gouv.qc.ca/"}];
 	
 	if(spots.length == 1){
 		document.getElementById("hotspot-js").innerHTML += "<span style=\"font-size: 14pt\"><b>"+spots.length+ " hotspot is near this station.</b></span><br/><br/>";
