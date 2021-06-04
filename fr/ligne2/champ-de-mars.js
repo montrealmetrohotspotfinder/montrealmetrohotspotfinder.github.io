c121350f7db8713a0bfe3eeac952d7168cad0987
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-06-04 16:39:36 -0400

    Update champ-de-mars.js

--- fr/ligne2/champ-de-mars.js
+++ fr/ligne2/champ-de-mars.js
@@ -3,14 +3,14 @@ function init()
 	hnames = [ "Vieux Montr&eacute;al", "Vieux Port de Montr&eacute;al", "Place des Montr&eacute;alaises", "CHUM Saint-Luc", "Quartier Chinois", "Place Vauquelin", "Champ de Mars", "H&ocirc;tel de Ville de Montr&eacute;al", "&Eacute;difice Lucien-Saulnier", "Ch&acirc;teau Ramezay",
 	"March&eacute; Bonsecours", "Terrasses Bonsecours", "Chappelle Notre-Dame-de-Bon-Secours", "Place Jacques-Cartier", "Quai Jacques-Cartier", "Quai de l&#39;Horloge", "Bassin Bonsecours", 
 	"H&ocirc;tel William Gray", "Maggie Oakes", "BEVO Bar + Pizzeria", "Vieux-Port Steakhouse", "Auberge Saint-Gabriel", "Jardin Nelson", "Rue Saint-Amable", "Place de la Dauversi&egrave;re",
-	"Parc Fleury-Mesplet", "Le Solano", "Parc de la Chasse-Galerie", "Place Victor-Morin", "Parc Fran&ccedil;ois-Dollier-De Casson", "H&ocirc;pital Chinois de Montr&eacute;al" ];
+	"Parc Fleury-Mesplet", "Le Solano", "Parc de la Chasse-Galerie", "Place Victor-Morin", "Parc Fran&ccedil;ois-Dollier-De Casson", "H&ocirc;pital Chinois de Montr&eacute;al", "Perch&eacute;" ];
 	
 	htypes = [ "District", "Port", "Parc", "H&ocirc;pital", "District", "Place de la Ville", "Site Historique", "Gouvernement", "Gouvernement", "Site Historique", "Magasinage", "Restaurant", "&Eacute;glise", "District", "Port", "Port", "Lac",
-	"H&ocirc;tel", "Restaurant", "Restaurant", "Restaurant", "Restaurant", "Restaurant", "Site Historique", "Parc", "Parc", "R&eacute;sidence", "Parc", "Parc", "Parc", "H&ocirc;pital" ];
+	"H&ocirc;tel", "Restaurant", "Restaurant", "Restaurant", "Restaurant", "Restaurant", "Site Historique", "Parc", "Parc", "R&eacute;sidence", "Parc", "Parc", "Parc", "H&ocirc;pital", "Restaurant" ];
 	
 	haddresses = [ "", "333 Rue de la Commune Ouest", "", "1051 Rue Sanguinet", "", "275 Rue Notre-Dame Est", "158 Rue Saint-Antoine Est", "275 Rue Notre-Dame Est", "155 Rue Notre-Dame Est", "280 Rue Notre-Dame Est",
 	"350 Rue Saint-Paul Est", "364 Rue de la Commune Est", "400 Rue Saint-Paul Est", "", "", "", "", "421 Rue Saint-Vincent", "426 Place Jacques-Cartier", "410 Rue Saint-Vincent", "39 Rue Saint-Paul Est",
-	"426 Rue Saint-Gabriel", "407 Place Jacques-Cartier", "", "", "", "901 Rue de la Commune Est", "", "", "", "189 Avenue Viger Est" ];
+	"426 Rue Saint-Gabriel", "407 Place Jacques-Cartier", "", "", "", "901 Rue de la Commune Est", "", "", "", "189 Avenue Viger Est", "153 Rue Saint-Amable" ];
 	
 	hsites = [ "https://vieuxmontreal.ca/", "https://www.vieuxportdemontreal.com/", "https://montreal.ca/lieux/place-des-montrealaises", "https://www.chumontreal.qc.ca/", "https://www.mtl.org/fr/experience/quartier-chinois-montreal", 
 	"https://montreal.ca/lieux/place-vauquelin", "https://montreal.ca/lieux/le-champ-de-mars", "https://montreal.ca/lieux/hotel-de-ville", "https://montreal.ca/lieux/edifice-lucien-saulnier", "https://www.chateauramezay.qc.ca/fr/", 
@@ -18,7 +18,7 @@ function init()
 	"http://www.patrimoine-culturel.gouv.qc.ca/rpcq/detail.do?methode=consulter&id=115163&type=bien#.X390DtVKiM8", "http://www.patrimoine-culturel.gouv.qc.ca/detail.do?methode=consulter&id=115167&type=bien#.X390UtVKiM8",
 	"https://www.societeduvieuxport.com/activite/bassin-bonsecours", "https://hotelwilliamgray.com/fr", "https://maggieoakes.com", "https://bevopizza.com", "https://vieuxportsteakhouse.com",
 	"http://aubergesaint-gabriel.com", "https://jardinnelson.com/", "https://vieuxmontreal.ca/publications/il-etait-une-fois-la-rue-saint-amable", "https://vieuxmontreal.ca/commerces/place-de-la-dauversiere", "https://montreal.ca/lieux/parc-fleury-mesplet",
-	"https://www.lesolano.com/", "https://montreal.ca/lieux/parc-de-la-chasse-galerie", "https://montreal.ca/lieux/place-victor-morin", "https://montreal.ca/lieux/parc-francois-dollier-de-casson", "http://www.montrealchinesehospital.ca/" ];
+	"https://www.lesolano.com/", "https://montreal.ca/lieux/parc-de-la-chasse-galerie", "https://montreal.ca/lieux/place-victor-morin", "https://montreal.ca/lieux/parc-francois-dollier-de-casson", "http://www.montrealchinesehospital.ca/", "https://perchemtl.com/" ];
 	
 	if(hnames.length == 1){
 		document.getElementById("hotspot-js").innerHTML += "<span style=\"font-size: 14pt\"><em lang=\"fr\"><b>"+hnames.length+ " point d&#39;int&eacute;r&ecirc;t est &agrave; proximit&eacute; de cette station.</b></em></span><br/><br/>";
