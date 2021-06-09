2e777d4305c9fc0aab5ff3da6a1e9855c5cf8b77
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-06-09 01:38:05 -0400

    Update peel.js

--- fr/ligne1/peel.js
+++ fr/ligne1/peel.js
@@ -57,14 +57,21 @@ function init()
 		"Universit&eacute; McGill, D&eacute;partement de Biologie",
 		"Universit&eacute; McGill, D&eacute;partement de Pharmacologie et Th&eacute;rapeutiques",
 		"Universit&eacute; McGill, D&eacute;partement de Physiologie",
-		"H&ocirc;tel Chez Swann"];
+		"H&ocirc;tel Chez Swann",
+		"Chez Alexandre",
+		"Ferreira Caf&eacute;",
+		"Ib&eacute;rica",
+		"Caf&eacute; Vasco Da Gama",
+		"Zante",
+		"Campo" ];
 	
 	htypes = [ "Centre Commercial", "Centre Commercial", "Cin&eacute;ma", "Bureau", "Bureau", "Universit&eacute;", 
 	"Centre Commercial", "R&eacute;sidence", "Fitness", "Restaurant", "Restaurant", 
 	"Place de la Ville", "Sports", "Mus&eacute;e", "Magasinage", "Bureau", "H&ocirc;tel", "H&ocirc;tel", "H&ocirc;tel", "H&ocirc;tel", "H&ocirc;tel", "H&ocirc;tel", "H&ocirc;tel", "H&ocirc;tel", "Agence", "Bureau",
 	"H&ocirc;tel", "H&ocirc;tel", "H&ocirc;tel", "Universit&eacute;", "Biblioth&egrave;que", "Restaurant", "Magasinage", "Bureau", "R&eacute;sidence", "Bureau", "Bureau", "Bureau", "Bureau", "Studio",
 	"Restaurant", "Restaurant", "H&ocirc;tel", "H&ocirc;tel", "R&eacute;sidence", "R&eacute;sidence", "Universit&eacute;", "Universit&eacute;", "Universit&eacute;", "R&eacute;sidence", "R&eacute;sidence",
-	"R&eacute;sidence", "Bureau", "Bureau", "Universit&eacute;", "Universit&eacute;", "Universit&eacute;", "H&ocirc;tel" ];
+	"R&eacute;sidence", "Bureau", "Bureau", "Universit&eacute;", "Universit&eacute;", "Universit&eacute;", "H&ocirc;tel",
+	"Restaurant", "Restaurant", "Restaurant", "Restaurant", "Restaurant", "Restaurant" ];
 	
 	haddresses = [ "1455 Rue Peel", 
 	"977 Rue Sainte-Catherine Ouest",
@@ -123,7 +130,13 @@ function init()
 	"1205 Avenue du Docteur-Penfield",
 	"3655 Promenade Sir-William-Osler",
 	"3655 Promenade Sir-William-Osler",
-	"1444 Rue Drummond" ];
+	"1444 Rue Drummond",
+	"1454 Rue Peel",
+	"1446 Rue Peel",
+	"1450 Rue Peel",
+	"1472 Rue Peel",
+	"1112 Rue Sherbrooke Ouest",
+	"1108 Boulevard de Maisonneuve Ouest" ];
 	
 	hsites = [ "https://lcmr.ca/fr/accueil/", "http://www.carrefourindustriellealliance.com/", "https://www.cineplex.com/Cinemas/theatredetails2017?theatre_url=cinema-banque-scotia-montreal", "#", "#", 
 	"https://www.mcgill.ca/fr", "https://www.placemontrealtrust.com/fr", "https://www.gwlraresidential.com/apartments/qc/montreal/le-demetrius/index.aspx?_yTrackUser=OTQxNzA5NjYwIzE5MTk5NTcxODg%3d-DwVHUNDST0M%3d&_yTrackVisit=MTY5OTE0MzIwOSMyMDE4MTk4NTE2-%2fRw0D2%2biQPY%3d&_yTrackReqDT=08380320201210&lang=38", "https://www.ymcaquebec.org/fr/Trouver-un-Y/YMCA-Centre-ville", 
@@ -135,7 +148,7 @@ function init()
 	"http://virtualcampustour.mcgill.ca/fr/details/20/pavillon-bronfman", "https://www.mcgill.ca/library/", "http://reubensdeli.com/fr/", "https://www.canadagoose.com/ca/fr/montreal-store.html", "https://www.ccifcmtl.ca/", "http://downtowncondosmontreal.com/1200-de-maisonneuve/", "https://www.procomservices.com/fr-ca/", "https://ocpm.qc.ca/", "https://cmm.qc.ca/", "https://montrealgazette.com/",
 	"https://www.reflectorentertainment.com/fr/", "https://mcleanspub.com/fr/", "#", "https://www.fourseasons.com/fr/montreal/?seo=google_local_mqu1_amer", "https://www.bestwestern.com/fr_FR/book/hotel-rooms.67013.html?iata=00171880&ssob=BLBWI0004G&cid=BLBWI0004G:google:gmb:67013", "http://www.stanleytower.com/?lang=fr", "https://lesommet3475.com/fr/", "https://www.mcgill.ca/medhealthsci/fr",
 	"https://www.mcgill.ca/law/fr", "https://www.mcgill.ca/education/fr", "http://www.appartementsluna.com/", "https://www.groupeaccueilres.ca/royal-penfield", "http://www.mcgillimmobilier.com/condos-drummond-au-centre-ville-de-montreal/", "https://www.coastunderwriters.ca/", "https://www.theratech.com/fr/", "https://www.mcgill.ca/biology/", "https://www.mcgill.ca/pharma/", "https://www.mcgill.ca/physiology/",
-	"http://hotelchezswann.com/fr" ];
+	"http://hotelchezswann.com/fr", "https://www.chezalexandre.com/", "https://ferreiracafe.com/fr/", "http://www.iberica.ca/", "https://vascodagama.ca/fr/", "https://www.restaurantzante.ca/?lang=fr", "https://campomtl.com/fr/" ];
 	
 	if(hnames.length == 1){
 		document.getElementById("hotspot-js").innerHTML += "<span style=\"font-size: 14pt\"><em lang=\"fr\"><b>"+hnames.length+ " point d&#39;int&eacute;r&ecirc;t est &agrave; proximit&eacute; de cette station.</b></em></span><br/><br/>";
