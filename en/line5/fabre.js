bc31ef754e4665372e58e279ba1a9f19ca3bd283
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-06-13 17:23:40 -0400

    Update fabre.js

--- en/line5/fabre.js
+++ en/line5/fabre.js
@@ -4,7 +4,8 @@ function init()
 	{name: "H&ocirc;pital Jean-Talon", type: "Hospital", address: "1385 Rue Jean-Talon Est", site: "https://www.ciusssnordmtl.ca/installations/hopitaux/hopital-jean-talon/"},
 	{name: "Parc de Turin", type: "Park", address: "7205 Rue Chambord", site: "https://montreal.ca/en/places/parc-de-turin"},
 	{name: "&Eacute;cole Saint-Gabriel-Lalemant", type: "School", address: "7350 Rue Garnier", site: "https://st-gabriel-lalemant.csdm.ca/"},
-	{name: "&Eacute;cole Pierre Elliott Trudeau", type: "School", address: "6855 Rue Cartier", site: "https://www.emsb.qc.ca/trudeau"}];
+	{name: "&Eacute;cole Pierre Elliott Trudeau", type: "School", address: "6855 Rue Cartier", site: "https://www.emsb.qc.ca/trudeau"},
+	{name: "La Baie des Fromages", type: "Shopping", address: "1715 Rue Jean-Talon Est", site: "https://en.labaiadeiformaggi.com/"}];
 	
 	if(spots.length == 1){
 		document.getElementById("hotspot-js").innerHTML += "<span style=\"font-size: 14pt\"><b>"+spots.length+ " hotspot is near this station.</b></span><br/><br/>";
