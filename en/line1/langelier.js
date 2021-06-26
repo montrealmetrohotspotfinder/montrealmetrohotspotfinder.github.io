27f4ab2ff427f95ef3fc1b669736bdec3219dd4e
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-06-25 20:28:27 -0400

    Update langelier.js

--- en/line1/langelier.js
+++ en/line1/langelier.js
@@ -12,7 +12,13 @@ function init()
 	{name: "Parc Mignault", type: "Park", address: "", site: "https://montreal.ca/en/places/parc-mignault"},
 	{name: "Centre R&eacute;cr&eacute;atif et Culturel Saint-Donat", type: "Cultural Centre", address: "6547 Rue de Marseille", site: "https://crcstdonat.org/"},
 	{name: "&Eacute;cole Saint-Donat", type: "School", address: "3155 Rue Desautels", site: "https://st-donat.cssdm.gouv.qc.ca/"},
-	{name: "&Eacute;glise Saint-Donat de Montr&eacute;al", type: "Church", address: "6805 Rue de Marseille", site: "https://www.eglisesaintdonatdemontreal.com/"}];
+	{name: "&Eacute;glise Saint-Donat de Montr&eacute;al", type: "Church", address: "6805 Rue de Marseille", site: "https://www.eglisesaintdonatdemontreal.com/"},
+	{name: "&Eacute;cole Louis-Dupire", type: "School", address: "6400 Avenue Pierre-de-Coubertin", site: "https://louis-dupire.cssdm.gouv.qc.ca/"},
+	{name: "Les Habitations Le Domaine", type: "Residence", address: "2990 Avenue de Granby", site: "https://www.ledomaine.ca/"},
+	{name: "&Eacute;cole Saint-Fabien", type: "School", address: "6500 Avenue de Renty", site: "https://st-fabien.cssdm.gouv.qc.ca/"},
+	{name: "Paroisse Saint-Fabien", type: "Church", address: "6455 Avenue de Renty", site: "http://www.saintfabien.org/Page00.html"},
+	{name: "Parc F&eacute;lix-Leclerc", type: "Park", address: "6700 Rue Fran&ccedil;ois-Boivin", site: "https://montreal.ca/en/places/parc-felix-leclerc"},
+	{name: "Edward Murphy School", type: "School", address: "6800 Avenue Pierre-de-Coubertin", site: "http://edwardmurphy.emsb.qc.ca/"}];
 	
 	if(spots.length == 1){
 		document.getElementById("hotspot-js").innerHTML += "<span style=\"font-size: 14pt\"><b>"+spots.length+ " hotspot is near this station.</b></span><br/><br/>";
