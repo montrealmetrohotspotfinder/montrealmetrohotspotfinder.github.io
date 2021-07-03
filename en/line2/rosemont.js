a00614bee8b1955d257c5f21ace9c8453467c90a
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-07-03 13:30:20 -0400

    Update rosemont.js

--- en/line2/rosemont.js
+++ en/line2/rosemont.js
@@ -6,7 +6,11 @@ function init()
 	{name: "Habitations Saint-Vallier", type: "Residence", address: "401 Rue des Carri&egrave;res", site: "https://www.omhm.qc.ca/en/housing-directory/habitations-saint-vallier"},
 	{name: "Universit&eacute; T&Eacute;LUQ, Montreal offices", type: "Office", address: "5800 Rue Saint-Denis", site: "https://www.teluq.ca/site/en/"},
 	{name: "Place Raymond-Plante", type: "Park", address: "780 Boulevard Rosemont", site: "https://montreal.ca/en/places/place-raymond-plante"},
-	{name: "Le Saint-&Eacute;douard", type: "Restaurant", address: "808 Boulevard Rosemont", site: "https://lesaintedouard.com/"}];
+	{name: "Le Saint-&Eacute;douard", type: "Restaurant", address: "808 Boulevard Rosemont", site: "https://lesaintedouard.com/"},
+	{name: "Parc du P&egrave;re-Marquette", type: "Park", address: "1602 Rue de Bellechasse", site: "https://montreal.ca/en/places/parc-du-pere-marquette"},
+	{name: "Centre P&egrave;re-Marquette", type: "Cultural Centre", address: "1600 Rue de Drucourt", site: "http://centreperemarquette.ca/"},
+	{name: "Ar&eacute;na P&egrave;re-Marquette", type: "Sports", address: "1605 Rue de Bellechasse", site: "https://montreal.ca/en/places/arena-pere-marquette"},
+	{name: "&Eacute;cole P&egrave;re-Marquette", type: "School", address: "6030 Rue Marquette", site: "https://pere-marquette.cssdm.gouv.qc.ca/"}];
 	
 	if(spots.length == 1){
 		document.getElementById("hotspot-js").innerHTML += "<span style=\"font-size: 14pt\"><b>"+spots.length+ " hotspot is near this station.</b></span><br/><br/>";
