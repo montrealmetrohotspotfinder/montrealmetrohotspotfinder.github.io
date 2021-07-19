83232302e3d0760acb02d92d23e071ac5ebc3c01
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-07-18 21:47:42 -0400

    Update lionel-groulx.js

--- en/transfers/lionel-groulx.js
+++ en/transfers/lionel-groulx.js
@@ -2,7 +2,11 @@ function init()
 {
 	spots = [{name: "March&eacute; Atwater", type: "Market", address: "138 Avenue Atwater", site: "https://www.marchespublics-mtl.com/en/marches/atwater-market/"},
 	{name: "Parc Vinet", type: "Park", address: "550 Rue Vinet", site: "https://montreal.ca/en/places/parc-vinet"},
-	{name: "Th&eacute;&acirc;tre Corona", type: "Theatre", address: "2490 Rue Notre-Dame Ouest", site: "https://www.theatrecorona.ca/en/"}];
+	{name: "Th&eacute;&acirc;tre Corona", type: "Theatre", address: "2490 Rue Notre-Dame Ouest", site: "https://www.theatrecorona.ca/en/"},
+	{name: "Parc Victor-Rousselot", type: "Park", address: "", site: "https://montreal.ca/en/places/parc-victor-rousselot"},
+	{name: "&Eacute;cole Victor-Rousselot", type: "School", address: "3525 Rue Sainte-&Eacute;milie", site: "https://victor-rousselot.cssdm.gouv.qc.ca/"},
+	{name: "Lemay, Montreal offices", type: "Office", address: "3500 Rue Saint-Jacques", site: "https://lemay.com/"},
+	{name: "&Eacute;glise Saint-Ir&eacute;n&eacute;e-de-Lyon", type: "Church", address: "3030 Rue Delisle", site: "http://st-irenee.org/accueil"}];
 					
 	if(spots.length == 1){
 		document.getElementById("hotspot-js").innerHTML += "<span style=\"font-size: 14pt\"><b>"+spots.length+ " hotspot is near this station.</b></span><br/><br/>";
