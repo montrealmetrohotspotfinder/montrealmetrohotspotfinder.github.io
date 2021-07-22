a5ae5c68231f8a10bb640b603eb99e17b94e2522
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-07-22 17:05:12 -0400

    Update square-victoria-oaci.js

--- en/line2/square-victoria-oaci.js
+++ en/line2/square-victoria-oaci.js
@@ -49,7 +49,8 @@ function init()
 	{name: "Alstom", type: "Office", address: "1050 C&ocirc;te du Beaver Hall", site: "https://www.alstom.com/"},
 	{name: "Place du Fr&egrave;re-Andr&eacute;", type: "Park", address: "", site: "https://montreal.ca/en/places/place-du-frere-andre"},
 	{name: "Toqu&eacute;!", type: "Restaurant", address: "900 Place Jean-Paul-Riopelle", site: "https://www.restaurant-toque.com/en"},
-	{name: "Terrasse W Montr&eacute;al", type: "Restaurant", address: "901 Rue du Square-Victoria", site: "https://www.marriott.com/hotels/hotel-information/restaurant/details/yulwh-w-montreal/6760117/"}];
+	{name: "Terrasse W Montr&eacute;al", type: "Restaurant", address: "901 Rue du Square-Victoria", site: "https://www.marriott.com/hotels/hotel-information/restaurant/details/yulwh-w-montreal/6760117/"},
+	{name: "H&ocirc;tel St-Paul", type: "Hotel", address: "355 Rue McGill", site: "https://hotelstpaul.com/en/"}];
 	
 	if(spots.length == 1){
 		document.getElementById("hotspot-js").innerHTML += "<span style=\"font-size: 14pt\"><b>"+spots.length+ " hotspot is near this station.</b></span><br/><br/>";
