a3a2e77de7f6277e749151aa7c7c262e9e64d0ae
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-07-30 02:29:25 -0400

    Update laurier.js

--- fr/ligne2/laurier.js
+++ fr/ligne2/laurier.js
@@ -1,16 +1,16 @@
 function init()
 {
 	hnames = [ "Spectrum Productions", "Parc Sir-Wilfrid-Laurier", "&Eacute;cole Nationale de Th&eacute;&acirc;tre", "&Eacute;cole Sup&eacute;rieure de Ballet du Qu&eacute;bec",
-	"&Eacute;glise Catholique Saint-Denis", "&Eacute;cole Laurier", "Centre Champagnat", "Parc Berri-Saint-Joseph", "Parc Lahaie", "Parc Saint-Michel", "&Eacute;cole Saint-Enfant-J&eacute;sus" ];
+	"&Eacute;glise Catholique Saint-Denis", "&Eacute;cole Laurier", "Centre Champagnat", "Parc Berri-Saint-Joseph", "Parc Lahaie", "Parc Saint-Michel", "&Eacute;cole Saint-Enfant-J&eacute;sus", "Faberg&eacute;" ];
 	
-	htypes = [ "Studio", "Parc", "&Eacute;cole", "&Eacute;cole", "&Eacute;glise", "&Eacute;cole", "&Eacute;cole", "Parc", "Parc", "Parc", "&Eacute;cole" ];
+	htypes = [ "Studio", "Parc", "&Eacute;cole", "&Eacute;cole", "&Eacute;glise", "&Eacute;cole", "&Eacute;cole", "Parc", "Parc", "Parc", "&Eacute;cole", "Restaurant" ];
 	
 	haddresses = [ "5445 Avenue de Gasp&eacute;", "1115 Avenue Laurier Est", "5030 Rue Saint-Denis", "4816 Rue Rivard", "454 Avenue Laurier Est", "505 Avenue Laurier Est", 
-		      "5017 Rue Saint-Hubert", "", "4921 Boulevard Saint-Laurent", "", "75 Rue Villeneuve Est" ];
+		      "5017 Rue Saint-Hubert", "", "4921 Boulevard Saint-Laurent", "", "75 Rue Villeneuve Est", "25 Avenue Fairmount Ouest" ];
 	
 	hsites = [ "https://www.productionsspectrum.com/home-fr", "https://montreal.ca/lieux/parc-sir-wilfrid-laurier", "https://ent-nts.ca/fr", "https://www.esbq.ca/fr", 
 	"https://sites.google.com/view/eglisesaintdenismontreal/accueil", "https://laurier.cssdm.gouv.qc.ca/", "https://centre-champagnat.cssdm.gouv.qc.ca/", "https://montreal.ca/lieux/parc-berri-saint-joseph",
-	"https://montreal.ca/lieux/parc-lahaie", "https://montreal.ca/lieux/parc-saint-michel", "https://st-enfant-jesus.cssdm.gouv.qc.ca/" ];
+	"https://montreal.ca/lieux/parc-lahaie", "https://montreal.ca/lieux/parc-saint-michel", "https://st-enfant-jesus.cssdm.gouv.qc.ca/", "https://www.faberge514.com/accueil" ];
 	
 	if(hnames.length == 1){
 		document.getElementById("hotspot-js").innerHTML += "<span style=\"font-size: 14pt\"><em lang=\"fr\"><b>"+hnames.length+ " point d&#39;int&eacute;r&ecirc;t est &agrave; proximit&eacute; de cette station.</b></em></span><br/><br/>";
