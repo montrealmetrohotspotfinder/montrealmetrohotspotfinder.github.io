3711a3c2f41478af263b4a183b1c0d4af089651a
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-07-30 01:30:40 -0400

    Update recherche.js

--- fr/recherche.js
+++ fr/recherche.js
@@ -1238,15 +1238,16 @@ function init(){
 	"https://www.teluq.ca/", "https://montreal.ca/lieux/place-raymond-plante", "https://lesaintedouard.com/", "https://montreal.ca/lieux/parc-du-pere-marquette", "http://centreperemarquette.ca/", "https://montreal.ca/lieux/arena-pere-marquette", "https://pere-marquette.cssdm.gouv.qc.ca/" ];
 	
 	Lauriernames = [ "Spectrum Productions", "Parc Sir-Wilfrid-Laurier", "&Eacute;cole Nationale de Th&eacute;&acirc;tre", "&Eacute;cole Sup&eacute;rieure de Ballet du Qu&eacute;bec",
-	"&Eacute;glise Catholique Saint-Denis", "&Eacute;cole Laurier", "Centre Champagnat", "Parc Berri-Saint-Joseph" ];
+	"&Eacute;glise Catholique Saint-Denis", "&Eacute;cole Laurier", "Centre Champagnat", "Parc Berri-Saint-Joseph", "Parc Lahaie", "Parc Saint-Michel", "&Eacute;cole Saint-Enfant-J&eacute;sus" ];
 	
-	Lauriertypes = [ "Studio", "Parc", "&Eacute;cole", "&Eacute;cole", "&Eacute;glise", "&Eacute;cole", "&Eacute;cole", "Parc" ];
+	Lauriertypes = [ "Studio", "Parc", "&Eacute;cole", "&Eacute;cole", "&Eacute;glise", "&Eacute;cole", "&Eacute;cole", "Parc", "Parc", "Parc", "&Eacute;cole" ];
 	
 	Laurieraddresses = [ "5445 Avenue de Gasp&eacute;", "1115 Avenue Laurier Est", "5030 Rue Saint-Denis", "4816 Rue Rivard", "454 Avenue Laurier Est", "505 Avenue Laurier Est", 
-		      "5017 Rue Saint-Hubert", "" ];
+		      "5017 Rue Saint-Hubert", "", "4921 Boulevard Saint-Laurent", "", "75 Rue Villeneuve Est" ];
 	
 	Lauriersites = [ "https://www.productionsspectrum.com/home-fr", "https://montreal.ca/lieux/parc-sir-wilfrid-laurier", "https://ent-nts.ca/fr", "https://www.esbq.ca/fr", 
-	"https://sites.google.com/view/eglisesaintdenismontreal/accueil", "https://laurier.cssdm.gouv.qc.ca/", "https://centre-champagnat.cssdm.gouv.qc.ca/", "https://montreal.ca/lieux/parc-berri-saint-joseph" ];
+	"https://sites.google.com/view/eglisesaintdenismontreal/accueil", "https://laurier.cssdm.gouv.qc.ca/", "https://centre-champagnat.cssdm.gouv.qc.ca/", "https://montreal.ca/lieux/parc-berri-saint-joseph",
+	"https://montreal.ca/lieux/parc-lahaie", "https://montreal.ca/lieux/parc-saint-michel", "https://st-enfant-jesus.cssdm.gouv.qc.ca/" ];
 	
 	MontRoyalnames = [ "Parc Mont-Royal", "L&#39;Avenue du Mont-Royal", "Th&eacute;&acirc;tre du Rideau Vert", "Biblioth&egrave;que du Plateau-Mont-Royal", "Sanctuaire du Saint-Sacrement", "Parc Albert-Saint-Martin",
 		 "Place G&eacute;rald-Godin", "Relais Mont-Royal", "Parc Denise-Morelle", "Parc &Eacute;mile-Nelligan", "Parc Gilles-Lefebvre", "Parc Pierre-Boucher", 
