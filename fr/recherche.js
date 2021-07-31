ccec7914fbcacf6aaf986495d724fc3de126e797
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-07-31 03:09:58 -0400

    Update recherche.js

--- fr/recherche.js
+++ fr/recherche.js
@@ -1238,16 +1238,17 @@ function init(){
 	"https://www.teluq.ca/", "https://montreal.ca/lieux/place-raymond-plante", "https://lesaintedouard.com/", "https://montreal.ca/lieux/parc-du-pere-marquette", "http://centreperemarquette.ca/", "https://montreal.ca/lieux/arena-pere-marquette", "https://pere-marquette.cssdm.gouv.qc.ca/" ];
 	
 	Lauriernames = [ "Spectrum Productions", "Parc Sir-Wilfrid-Laurier", "&Eacute;cole Nationale de Th&eacute;&acirc;tre", "&Eacute;cole Sup&eacute;rieure de Ballet du Qu&eacute;bec",
-	"&Eacute;glise Catholique Saint-Denis", "&Eacute;cole Laurier", "Centre Champagnat", "Parc Berri-Saint-Joseph", "Parc Lahaie", "Parc Saint-Michel", "&Eacute;cole Saint-Enfant-J&eacute;sus", "Faberg&eacute;" ];
+	"&Eacute;glise Catholique Saint-Denis", "&Eacute;cole Laurier", "Centre Champagnat", "Parc Berri-Saint-Joseph", "Parc Lahaie", "Parc Saint-Michel", "&Eacute;cole Saint-Enfant-J&eacute;sus", "Faberg&eacute;",
+	"Fairmount Bagel" ];
 	
-	Lauriertypes = [ "Studio", "Parc", "&Eacute;cole", "&Eacute;cole", "&Eacute;glise", "&Eacute;cole", "&Eacute;cole", "Parc", "Parc", "Parc", "&Eacute;cole", "Restaurant" ];
+	Lauriertypes = [ "Studio", "Parc", "&Eacute;cole", "&Eacute;cole", "&Eacute;glise", "&Eacute;cole", "&Eacute;cole", "Parc", "Parc", "Parc", "&Eacute;cole", "Restaurant", "Restaurant" ];
 	
 	Laurieraddresses = [ "5445 Avenue de Gasp&eacute;", "1115 Avenue Laurier Est", "5030 Rue Saint-Denis", "4816 Rue Rivard", "454 Avenue Laurier Est", "505 Avenue Laurier Est", 
-		      "5017 Rue Saint-Hubert", "", "4921 Boulevard Saint-Laurent", "", "75 Rue Villeneuve Est", "25 Avenue Fairmount Ouest" ];
+		      "5017 Rue Saint-Hubert", "", "4921 Boulevard Saint-Laurent", "", "75 Rue Villeneuve Est", "25 Avenue Fairmount Ouest", "74 Avenue Fairmount Ouest" ];
 	
 	Lauriersites = [ "https://www.productionsspectrum.com/home-fr", "https://montreal.ca/lieux/parc-sir-wilfrid-laurier", "https://ent-nts.ca/fr", "https://www.esbq.ca/fr", 
 	"https://sites.google.com/view/eglisesaintdenismontreal/accueil", "https://laurier.cssdm.gouv.qc.ca/", "https://centre-champagnat.cssdm.gouv.qc.ca/", "https://montreal.ca/lieux/parc-berri-saint-joseph",
-	"https://montreal.ca/lieux/parc-lahaie", "https://montreal.ca/lieux/parc-saint-michel", "https://st-enfant-jesus.cssdm.gouv.qc.ca/", "https://www.faberge514.com/accueil" ];
+	"https://montreal.ca/lieux/parc-lahaie", "https://montreal.ca/lieux/parc-saint-michel", "https://st-enfant-jesus.cssdm.gouv.qc.ca/", "https://www.faberge514.com/accueil", "https://fairmountbagel.com/?lang=fr" ];
 	
 	MontRoyalnames = [ "Parc Mont-Royal", "L&#39;Avenue du Mont-Royal", "Th&eacute;&acirc;tre du Rideau Vert", "Biblioth&egrave;que du Plateau-Mont-Royal", "Sanctuaire du Saint-Sacrement", "Parc Albert-Saint-Martin",
 		 "Place G&eacute;rald-Godin", "Relais Mont-Royal", "Parc Denise-Morelle", "Parc &Eacute;mile-Nelligan", "Parc Gilles-Lefebvre", "Parc Pierre-Boucher", 
