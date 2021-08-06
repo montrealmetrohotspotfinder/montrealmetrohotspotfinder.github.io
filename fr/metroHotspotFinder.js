91a379ffc122f3d6d474a8782f35cf171677231c
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-08-06 00:08:07 -0400

    Update metroHotspotFinder.js

--- fr/metroHotspotFinder.js
+++ fr/metroHotspotFinder.js
@@ -9,7 +9,7 @@ function addEvent(obj, type, fn){
 
 function init(){
 	var d = new Date().toLocaleString("fr-CA", { dateStyle: "full", timeStyle: "medium" } );
-	document.getElementById("dateTime").innerHTML = "Il est pr&eacute;sentement "+d;
+	document.getElementById("dateTime").innerHTML = "Il est pr&eacute;sentement le "+d;
 }
 
 window.onload = init;
