8da264cea4e796c311943b6a577a9b5d858895a3
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-09-09 16:16:05 -0400

    Update search.js

--- en/search.js
+++ en/search.js
@@ -424,7 +424,9 @@ function init(){
 	{name: "Universit&eacute; du Qu&eacute;bec &agrave; Montr&eacute;al, Adrien-Pinard Pavilion", type: "University", address: "100 Rue Sherbrooke Ouest", site: "http://carte.uqam.ca/pavillon-su"},
 	{name: "Place du Cardinal P.-&Eacute;. L&eacute;ger", type: "Park", address: "", site: "#"},
 	{name: "Louis Boh&egrave;me", type: "Residence", address: "350 Boulevard de Maisonneuve Ouest", site: "https://www.louisboheme.com/en/"},
-	{name: "Le Concorde", type: "Residence", address: "441 Avenue du Pr&eacute;sident-Kennedy", site: "http://downtowncondosmontreal.com/le-concorde-2/"}];
+	{name: "Le Concorde", type: "Residence", address: "441 Avenue du Pr&eacute;sident-Kennedy", site: "http://downtowncondosmontreal.com/le-concorde-2/"},
+	{name: "Le Peterson", type: "Residence", address: "405 Rue de la Concorde", site: "https://lepeterson.com/"},
+	{name: "Le Smith", type: "Residence", address: "2100 Rue de Bleury", site: "https://www.summitmanagement.ca/apartments/le-smith"}];
 	
 	McGill = [{name: "McGill University", type: "University", address: "845 Rue Sherbrooke Ouest", site: "https://www.mcgill.ca/"},
 	{name: "Centre Eaton de Montr&eacute;al", type: "Mall", address: "705 Rue Sainte-Catherine Ouest", site: "https://www.centreeatondemontreal.com/"},
