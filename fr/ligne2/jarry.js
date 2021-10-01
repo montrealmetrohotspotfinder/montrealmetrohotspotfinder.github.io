8890ea3f9d18b5b23696dfb3c6b4a04b9901665c
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-10-01 16:02:30 -0400

    Update jarry.js

--- fr/ligne2/jarry.js
+++ fr/ligne2/jarry.js
@@ -1,14 +1,14 @@
 function init()
 {
 	hnames = [ "Parc Jarry", "Stade IGA", "Parc Saint-Vincent-Ferrier", "Parc G&eacute;rard-Lalonde", "Paroisse Saint-Vincent-Ferrier", "&Eacute;cole H&eacute;l&egrave;ne-Boull&eacute;",
-		 "Centre Sainte-Croix" ];
+		 "Centre Sainte-Croix", "Qu&eacute;bec Deli" ];
 	
-	htypes = [ "Parc", "Sports", "Parc", "Parc", "&Eacute;glise", "&Eacute;cole", "&Eacute;cole" ];
+	htypes = [ "Parc", "Sports", "Parc", "Parc", "&Eacute;glise", "&Eacute;cole", "&Eacute;cole", "Restaurant" ];
 	
-	haddresses = [ "285 Rue Gary-Carter", "285 Rue Gary-Carter", "8172 Rue Drolet", "375 Rue Jarry Est", "301 Rue Jarry Est", "8050 Avenue de Gasp&eacute;", "85 Rue Jarry Ouest" ];
+	haddresses = [ "285 Rue Gary-Carter", "285 Rue Gary-Carter", "8172 Rue Drolet", "375 Rue Jarry Est", "301 Rue Jarry Est", "8050 Avenue de Gasp&eacute;", "85 Rue Jarry Ouest", "590 Rue Jarry Est" ];
 	
 	hsites = [ "https://montreal.ca/lieux/parc-jarry", "http://stadeiga.com/", "https://montreal.ca/lieux/parc-saint-vincent-ferrier", "#", "https://diocesemontreal.org/fr/vie-communautaire/eglises-paroisses/saint-vincent-ferrier", "https://helene-boulle.cssdm.gouv.qc.ca/",
-		 "https://centre-ste-croix.cssdm.gouv.qc.ca/" ];
+		 "https://centre-ste-croix.cssdm.gouv.qc.ca/", "http://www.quebecdeli.ca/" ];
 	
 	if(hnames.length == 1){
 		document.getElementById("hotspot-js").innerHTML += "<span style=\"font-size: 14pt\"><em lang=\"fr\"><b>"+hnames.length+ " point d&#39;int&eacute;r&ecirc;t est &agrave; proximit&eacute; de cette station.</b></em></span><br/><br/>";
