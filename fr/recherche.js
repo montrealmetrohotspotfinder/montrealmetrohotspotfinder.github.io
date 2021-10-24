0dee932793ebcadc2e38ddb4249efc5530ff12b6
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-10-24 13:52:42 -0400

    Update recherche.js

--- fr/recherche.js
+++ fr/recherche.js
@@ -43,10 +43,11 @@ function init(){
 	"H&eacute;ritage Victorien",
 	"Cath&eacute;drale Saint-Jacques",
 	"Universit&eacute; du Qu&eacute;bec &agrave; Montr&eacute;al, Biblioth&egrave;que Centrale",
-	"Universit&eacute; du Qu&eacute;bec &agrave; Montr&eacute;al, Pavillon J.-A.-DeS&egrave;ve" ];
+	"Universit&eacute; du Qu&eacute;bec &agrave; Montr&eacute;al, Pavillon J.-A.-DeS&egrave;ve",
+	"Universit&eacute; du Qu&eacute;bec &agrave; Montr&eacute;al, Pavillon Saint-Denis" ];
 	
 	BerriUqamtypes = [ "Universit&eacute;", "Biblioth&egrave;que", "Terminus d&#39;Autobus", "Th&eacute;&acirc;tre", "Centre Commercial", "H&ocirc;tel", "Bureau", "Restaurant", "Parc", "District", "District", "H&ocirc;tel", "H&ocirc;tel", "H&ocirc;tel", "Coll&egrave;ge", "Parc", "&Eacute;glise", "R&eacute;sidence", "Mus&eacute;e", "&Eacute;cole", "Th&eacute;&acirc;tre", "Universit&eacute;", "Parc", "Universit&eacute;", "H&ocirc;tel",
-		 "H&ocirc;tel", "H&ocirc;tel", "H&ocirc;tel", "Gouvernement", "Universit&eacute;", "Parc", "Universit&eacute;", "Universit&eacute;", "Universit&eacute;", "Universit&eacute;", "H&ocirc;tel", "Sports", "Biblioth&egrave;que", "H&ocirc;tel", "H&ocirc;tel", "&Eacute;glise", "Biblioth&egrave;que", "Universit&eacute;" ];
+		 "H&ocirc;tel", "H&ocirc;tel", "H&ocirc;tel", "Gouvernement", "Universit&eacute;", "Parc", "Universit&eacute;", "Universit&eacute;", "Universit&eacute;", "Universit&eacute;", "H&ocirc;tel", "Sports", "Biblioth&egrave;que", "H&ocirc;tel", "H&ocirc;tel", "&Eacute;glise", "Biblioth&egrave;que", "Universit&eacute;", "Universit&eacute;" ];
 	
 	BerriUqamaddresses = [ "405 Rue Sainte-Catherine Est",
 	"475 Boulevard de Maisonneuve Est",
@@ -90,7 +91,8 @@ function init(){
 	"305 Rue Ontario Est",
 	"1455 Rue Saint-Denis",
 	"400 Rue Sainte-Catherine Est",
-	"320 Rue Sainte-Catherine Est" ];
+	"320 Rue Sainte-Catherine Est",
+	"1290 Rue Saint-Denis" ];
 	
 	BerriUqamsites = [ "http://www.uqam.ca/", 
 	"http://www.banq.qc.ca/accueil/", 
@@ -134,7 +136,8 @@ function init(){
 	"http://www.montrealbedandbreakfast.ca/index.html",
 	"#",
 	"https://bibliotheques.uqam.ca/bibliotheque/centrale/",
-	"https://carte.uqam.ca/pavillon-ds" ];
+	"https://carte.uqam.ca/pavillon-ds",
+	"https://carte.uqam.ca/pavillon-ab" ];
 	
 	JeanTalonnames = [ "Tour Jean-Talon", "March&eacute; Jean-Talon", "Plaza Saint-Hubert", "R&eacute;sidence Alfredo-Gagliardi", "Casa D&#39;Italia", "&Eacute;glise Orthodoxe Antiochienne Saint-Georges", "Parc Le Pr&eacute;vost", "Patro Villeray",
 	"&Eacute;cole Saint-Ars&egrave;ne", "&Eacute;glise Saint-Ars&egrave;ne", "&Eacute;glise Notre-Dame-de-la-D&eacute;fense", "Parc Dante", "Maison de la Culture Claude-L&eacute;veill&eacute;e" ];
