666136f60da06cf4736b371720a15220cec0a848
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2021-12-30 00:40:03 -0500

    Update saint-michel.js

--- en/line5/saint-michel.js
+++ en/line5/saint-michel.js
@@ -13,7 +13,8 @@ function init()
 	{name: "Parc du Ruisseau-du-Pont-&agrave;-l&#39;Avoine (formerly Parc du Bon-Air)", type: "Park", address: "", site: "https://montreal.ca/en/places/parc-du-ruisseau-du-pont-lavoine"},
 	{name: "Le Boulevard via Bus 141", type: "Mall", address: "4270 Rue Jean-Talon Est", site: "https://www.centreboulevard.com/en/"},
 	{name: "R&eacute;sidence Le St-Michel", type: "Residence", address: "7070 Boulevard Saint-Michel", site: "https://www.residencelesaintmichel.ca/en"},
-	{name: "&Eacute;glise de Dieu de la Proph&eacute;tie", type: "Church", address: "7277 Boulevard Saint-Michel", site: "https://egliselaprophetie.com/"}];
+	{name: "&Eacute;glise de Dieu de la Proph&eacute;tie", type: "Church", address: "7277 Boulevard Saint-Michel", site: "https://egliselaprophetie.com/"},
+	{name: "Best Western H&ocirc;tel Montr&eacute;al M&eacute;tropolitain", type: "Hotel", address: "3400 Boulevard Cr&eacute;mazie Est", site: "https://www.bestwestern.com/en_US/book/hotels-in-montreal/hotel-montreal-metropolitain-bw-premier-collection/propertyCode.67031.html"}];
 	
 	if(spots.length == 1){
 		document.getElementById("hotspot-js").innerHTML += "<span style=\"font-size: 14pt\"><b>"+spots.length+ " hotspot is near this station.</b></span><br/><br/>";
