2ff39abf94d7f675fda063b133d7625b3c680b32
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2022-03-13 03:59:47 -0400

    Update square-victoria-oaci.js

--- en/line2/square-victoria-oaci.js
+++ en/line2/square-victoria-oaci.js
@@ -56,7 +56,9 @@ function init()
 	{name: "Le Cartet Resto Boutique", type: "Restaurant", address: "106 Rue McGill", site: "https://lecartet.ca/indexen.html"},
 	{name: "Dandy", type: "Restaurant", address: "244 Rue Saint-Jacques", site: "http://dandymtl.com/"},
 	{name: "Le 409", type: "Restaurant", address: "409 Rue McGill", site: "https://le409.com/"},
-	{name: "Monarque", type: "Restaurant", address: "406 Rue Saint-Jacques", site: "https://restaurantmonarque.ca/en"}];
+	{name: "Monarque", type: "Restaurant", address: "406 Rue Saint-Jacques", site: "https://restaurantmonarque.ca/en"},
+	{name: "&Eacute;difice du Grand Tronc", type: "Government", address: "360 Rue McGill", site: "https://imtl.org/montreal/building/edifice_Grand_Tronc.php"},
+	{name: "Helena", type: "Restaurant", address: "438 Rue McGill", site: "https://restauranthelena.com/en/"}];
 	
 	if(spots.length == 1){
 		document.getElementById("hotspot-js").innerHTML += "<span style=\"font-size: 14pt\"><b>"+spots.length+ " hotspot is near this station.</b></span><br/><br/>";
