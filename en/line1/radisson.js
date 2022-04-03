0f2d046693717511224392cf1214d10611379413
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2022-04-03 01:11:59 -0400

    Update radisson.js

--- en/line1/radisson.js
+++ en/line1/radisson.js
@@ -42,6 +42,139 @@ function init()
 			+ "</td><td>" + "<a href=\"" + spots[i].site + "\">Website</a></td></tr></li>";
 		} 	
 	}
+	
+	/*PONT-TUNNEL LOUIS-HIPPOLYTE LA FONTAINE CLOSURE: EXO BUSES DO NOT OPERATE BETWEEN FRIDAYS 11:30PM TO MONDAYS 5:00AM*/
+	var d = new Date();
+	
+	/*LA FONTAINE REOPENS AT 5:00 AM ON MONDAYS; LA FONTAINE CLOSES AT 11:30 PM ON FRIDAYS*/
+	switch(d.getUTCDay()){
+		case 6:{ //SATURDAY
+			if(d.getUTCHours() == 3){
+				if(d.getUTCMinutes() >= 30){ //TUNNEL IS CLOSED
+					document.getElementById("connections-js").innerHTML = "<li><tr><td>26</td><td><a href=\"honore-beaugrand.html\">Honor&eacute;-Beaugrand</a> <span style=\"color:green\">&#x2B24;</span></td></tr></li><li><tr><td>44</td><td>Anjou</td></tr></li>"+
+					"<li><tr><td>185</td><td><a href=\"honore-beaugrand.html\">Honor&eacute;-Beaugrand</a> <span style=\"color:green\">&#x2B24;</span><br/><a href=\"langelier.html\">Langelier</a> <span style=\"color:green\">&#x2B24;</span><br/><a href=\"cadillac.html\">Cadillac</a> <span style=\"color:green\">&#x2B24;</span><br/><a href=\"frontenac.html\">Frontenac</a> <span style=\"color:green\">&#x2B24;</span></td></tr></li>"+
+					"<li><tr><td>432</td><td>Saint-L&eacute;onard</td></tr></li>"+
+					"<li><tr><td>444</td><td>C&eacute;gep Marie-Victorin</td></tr></li>"+
+					"<li><tr><td>448</td><td>Rivi&egrave;re-des-Prairies / Pointe-aux-Trembles</td></tr></li>"+
+					"<li><tr><td>449</td><td>Rivi&egrave;re-des-Prairies / Pointe-aux-Trembles</td></tr></li>"+
+					"<li><tr><td>RTL 61</td><td>Boucherville</td></tr></li>"+
+					"<li><tr><td>RTL 461</td><td>Boucherville</td></tr></li>"+
+					"<li><tr><td>STL 925</td><td>Laval</td></tr></li>"+
+					"<li><tr><td>CRT Lanaudi&egrave;re</td><td>Joliette</td></tr></li>"+
+					"<li><tr><td>MRC De L&#39;Assomption</td><td>L&#39;Assomption</td></tr></li>"+
+					"<li><tr><td>MRC Les Moulins</td><td>Mascouche</td></tr></li>;"
+				}
+				else { //TUNNEL IS OPEN
+					document.getElementById("connections-js").innerHTML = "<li><tr><td>26</td><td><a href=\"honore-beaugrand.html\">Honor&eacute;-Beaugrand</a> <span style=\"color:green\">&#x2B24;</span></td></tr></li><li><tr><td>44</td><td>Anjou</td></tr></li>"+
+					"<li><tr><td>185</td><td><a href=\"honore-beaugrand.html\">Honor&eacute;-Beaugrand</a> <span style=\"color:green\">&#x2B24;</span><br/><a href=\"langelier.html\">Langelier</a> <span style=\"color:green\">&#x2B24;</span><br/><a href=\"cadillac.html\">Cadillac</a> <span style=\"color:green\">&#x2B24;</span><br/><a href=\"frontenac.html\">Frontenac</a> <span style=\"color:green\">&#x2B24;</span></td></tr></li>"+
+					"<li><tr><td>432</td><td>Saint-L&eacute;onard</td></tr></li>"+
+					"<li><tr><td>444</td><td>C&eacute;gep Marie-Victorin</td></tr></li>"+
+					"<li><tr><td>448</td><td>Rivi&egrave;re-des-Prairies / Pointe-aux-Trembles</td></tr></li>"+
+					"<li><tr><td>449</td><td>Rivi&egrave;re-des-Prairies / Pointe-aux-Trembles</td></tr></li>"+
+					"<li><tr><td>EXO</td><td>Boucherville<br/>Beloeil<br/>Varennes<br/>Sainte-Julie</td></tr></li>"+
+					"<li><tr><td>RTL 61</td><td>Boucherville</td></tr></li>"+
+					"<li><tr><td>RTL 461</td><td>Boucherville</td></tr></li>"+
+					"<li><tr><td>STL 925</td><td>Laval</td></tr></li>"+
+					"<li><tr><td>CRT Lanaudi&egrave;re</td><td>Joliette</td></tr></li>"+
+					"<li><tr><td>MRC De L&#39;Assomption</td><td>L&#39;Assomption</td></tr></li>"+
+					"<li><tr><td>MRC Les Moulins</td><td>Mascouche</td></tr></li>;"
+				}
+			}
+			else if (d.getUTCHours() > 3){ //TUNNEL IS CLOSED
+				document.getElementById("connections-js").innerHTML = "<li><tr><td>26</td><td><a href=\"honore-beaugrand.html\">Honor&eacute;-Beaugrand</a> <span style=\"color:green\">&#x2B24;</span></td></tr></li><li><tr><td>44</td><td>Anjou</td></tr></li>"+
+				"<li><tr><td>185</td><td><a href=\"honore-beaugrand.html\">Honor&eacute;-Beaugrand</a> <span style=\"color:green\">&#x2B24;</span><br/><a href=\"langelier.html\">Langelier</a> <span style=\"color:green\">&#x2B24;</span><br/><a href=\"cadillac.html\">Cadillac</a> <span style=\"color:green\">&#x2B24;</span><br/><a href=\"frontenac.html\">Frontenac</a> <span style=\"color:green\">&#x2B24;</span></td></tr></li>"+
+				"<li><tr><td>432</td><td>Saint-L&eacute;onard</td></tr></li>"+
+				"<li><tr><td>444</td><td>C&eacute;gep Marie-Victorin</td></tr></li>"+
+				"<li><tr><td>448</td><td>Rivi&egrave;re-des-Prairies / Pointe-aux-Trembles</td></tr></li>"+
+				"<li><tr><td>449</td><td>Rivi&egrave;re-des-Prairies / Pointe-aux-Trembles</td></tr></li>"+
+				"<li><tr><td>RTL 61</td><td>Boucherville</td></tr></li>"+
+				"<li><tr><td>RTL 461</td><td>Boucherville</td></tr></li>"+
+				"<li><tr><td>STL 925</td><td>Laval</td></tr></li>"+
+				"<li><tr><td>CRT Lanaudi&egrave;re</td><td>Joliette</td></tr></li>"+
+				"<li><tr><td>MRC De L&#39;Assomption</td><td>L&#39;Assomption</td></tr></li>"+
+				"<li><tr><td>MRC Les Moulins</td><td>Mascouche</td></tr></li>;"
+			}
+			else { //TUNNEL IS OPEN
+				document.getElementById("connections-js").innerHTML = "<li><tr><td>26</td><td><a href=\"honore-beaugrand.html\">Honor&eacute;-Beaugrand</a> <span style=\"color:green\">&#x2B24;</span></td></tr></li><li><tr><td>44</td><td>Anjou</td></tr></li>"+
+				"<li><tr><td>185</td><td><a href=\"honore-beaugrand.html\">Honor&eacute;-Beaugrand</a> <span style=\"color:green\">&#x2B24;</span><br/><a href=\"langelier.html\">Langelier</a> <span style=\"color:green\">&#x2B24;</span><br/><a href=\"cadillac.html\">Cadillac</a> <span style=\"color:green\">&#x2B24;</span><br/><a href=\"frontenac.html\">Frontenac</a> <span style=\"color:green\">&#x2B24;</span></td></tr></li>"+
+				"<li><tr><td>432</td><td>Saint-L&eacute;onard</td></tr></li>"+
+				"<li><tr><td>444</td><td>C&eacute;gep Marie-Victorin</td></tr></li>"+
+				"<li><tr><td>448</td><td>Rivi&egrave;re-des-Prairies / Pointe-aux-Trembles</td></tr></li>"+
+				"<li><tr><td>449</td><td>Rivi&egrave;re-des-Prairies / Pointe-aux-Trembles</td></tr></li>"+
+				"<li><tr><td>EXO</td><td>Boucherville<br/>Beloeil<br/>Varennes<br/>Sainte-Julie</td></tr></li>"+
+				"<li><tr><td>RTL 61</td><td>Boucherville</td></tr></li>"+
+				"<li><tr><td>RTL 461</td><td>Boucherville</td></tr></li>"+
+				"<li><tr><td>STL 925</td><td>Laval</td></tr></li>"+
+				"<li><tr><td>CRT Lanaudi&egrave;re</td><td>Joliette</td></tr></li>"+
+				"<li><tr><td>MRC De L&#39;Assomption</td><td>L&#39;Assomption</td></tr></li>"+
+				"<li><tr><td>MRC Les Moulins</td><td>Mascouche</td></tr></li>;"
+			}
+			break;
+		}
+		case 0:{ //SUNDAY: TUNNEL IS CLOSED
+			document.getElementById("connections-js").innerHTML = "<li><tr><td>26</td><td><a href=\"honore-beaugrand.html\">Honor&eacute;-Beaugrand</a> <span style=\"color:green\">&#x2B24;</span></td></tr></li><li><tr><td>44</td><td>Anjou</td></tr></li>"+
+			"<li><tr><td>185</td><td><a href=\"honore-beaugrand.html\">Honor&eacute;-Beaugrand</a> <span style=\"color:green\">&#x2B24;</span><br/><a href=\"langelier.html\">Langelier</a> <span style=\"color:green\">&#x2B24;</span><br/><a href=\"cadillac.html\">Cadillac</a> <span style=\"color:green\">&#x2B24;</span><br/><a href=\"frontenac.html\">Frontenac</a> <span style=\"color:green\">&#x2B24;</span></td></tr></li>"+
+			"<li><tr><td>432</td><td>Saint-L&eacute;onard</td></tr></li>"+
+			"<li><tr><td>444</td><td>C&eacute;gep Marie-Victorin</td></tr></li>"+
+			"<li><tr><td>448</td><td>Rivi&egrave;re-des-Prairies / Pointe-aux-Trembles</td></tr></li>"+
+			"<li><tr><td>449</td><td>Rivi&egrave;re-des-Prairies / Pointe-aux-Trembles</td></tr></li>"+
+			"<li><tr><td>RTL 61</td><td>Boucherville</td></tr></li>"+
+			"<li><tr><td>RTL 461</td><td>Boucherville</td></tr></li>"+
+			"<li><tr><td>STL 925</td><td>Laval</td></tr></li>"+
+			"<li><tr><td>CRT Lanaudi&egrave;re</td><td>Joliette</td></tr></li>"+
+			"<li><tr><td>MRC De L&#39;Assomption</td><td>L&#39;Assomption</td></tr></li>"+
+			"<li><tr><td>MRC Les Moulins</td><td>Mascouche</td></tr></li>;"
+			break;
+		}
+		case 1:{ //MONDAY
+			if(d.getUTCHours() >= 9){ //TUNNEL IS OPEN
+				document.getElementById("connections-js").innerHTML = "<li><tr><td>26</td><td><a href=\"honore-beaugrand.html\">Honor&eacute;-Beaugrand</a> <span style=\"color:green\">&#x2B24;</span></td></tr></li><li><tr><td>44</td><td>Anjou</td></tr></li>"+
+				"<li><tr><td>185</td><td><a href=\"honore-beaugrand.html\">Honor&eacute;-Beaugrand</a> <span style=\"color:green\">&#x2B24;</span><br/><a href=\"langelier.html\">Langelier</a> <span style=\"color:green\">&#x2B24;</span><br/><a href=\"cadillac.html\">Cadillac</a> <span style=\"color:green\">&#x2B24;</span><br/><a href=\"frontenac.html\">Frontenac</a> <span style=\"color:green\">&#x2B24;</span></td></tr></li>"+
+				"<li><tr><td>432</td><td>Saint-L&eacute;onard</td></tr></li>"+
+				"<li><tr><td>444</td><td>C&eacute;gep Marie-Victorin</td></tr></li>"+
+				"<li><tr><td>448</td><td>Rivi&egrave;re-des-Prairies / Pointe-aux-Trembles</td></tr></li>"+
+				"<li><tr><td>449</td><td>Rivi&egrave;re-des-Prairies / Pointe-aux-Trembles</td></tr></li>"+
+				"<li><tr><td>EXO</td><td>Boucherville<br/>Beloeil<br/>Varennes<br/>Sainte-Julie</td></tr></li>"+
+				"<li><tr><td>RTL 61</td><td>Boucherville</td></tr></li>"+
+				"<li><tr><td>RTL 461</td><td>Boucherville</td></tr></li>"+
+				"<li><tr><td>STL 925</td><td>Laval</td></tr></li>"+
+				"<li><tr><td>CRT Lanaudi&egrave;re</td><td>Joliette</td></tr></li>"+
+				"<li><tr><td>MRC De L&#39;Assomption</td><td>L&#39;Assomption</td></tr></li>"+
+				"<li><tr><td>MRC Les Moulins</td><td>Mascouche</td></tr></li>;"
+			}
+			else { //TUNNEL IS CLOSED
+				document.getElementById("connections-js").innerHTML = "<li><tr><td>26</td><td><a href=\"honore-beaugrand.html\">Honor&eacute;-Beaugrand</a> <span style=\"color:green\">&#x2B24;</span></td></tr></li><li><tr><td>44</td><td>Anjou</td></tr></li>"+
+				"<li><tr><td>185</td><td><a href=\"honore-beaugrand.html\">Honor&eacute;-Beaugrand</a> <span style=\"color:green\">&#x2B24;</span><br/><a href=\"langelier.html\">Langelier</a> <span style=\"color:green\">&#x2B24;</span><br/><a href=\"cadillac.html\">Cadillac</a> <span style=\"color:green\">&#x2B24;</span><br/><a href=\"frontenac.html\">Frontenac</a> <span style=\"color:green\">&#x2B24;</span></td></tr></li>"+
+				"<li><tr><td>432</td><td>Saint-L&eacute;onard</td></tr></li>"+
+				"<li><tr><td>444</td><td>C&eacute;gep Marie-Victorin</td></tr></li>"+
+				"<li><tr><td>448</td><td>Rivi&egrave;re-des-Prairies / Pointe-aux-Trembles</td></tr></li>"+
+				"<li><tr><td>449</td><td>Rivi&egrave;re-des-Prairies / Pointe-aux-Trembles</td></tr></li>"+
+				"<li><tr><td>RTL 61</td><td>Boucherville</td></tr></li>"+
+				"<li><tr><td>RTL 461</td><td>Boucherville</td></tr></li>"+
+				"<li><tr><td>STL 925</td><td>Laval</td></tr></li>"+
+				"<li><tr><td>CRT Lanaudi&egrave;re</td><td>Joliette</td></tr></li>"+
+				"<li><tr><td>MRC De L&#39;Assomption</td><td>L&#39;Assomption</td></tr></li>"+
+				"<li><tr><td>MRC Les Moulins</td><td>Mascouche</td></tr></li>;"
+			}
+			break;
+		}
+		default:{ //TUNNEL IS OPEN
+			document.getElementById("connections-js").innerHTML = "<li><tr><td>26</td><td><a href=\"honore-beaugrand.html\">Honor&eacute;-Beaugrand</a> <span style=\"color:green\">&#x2B24;</span></td></tr></li><li><tr><td>44</td><td>Anjou</td></tr></li>"+
+			"<li><tr><td>185</td><td><a href=\"honore-beaugrand.html\">Honor&eacute;-Beaugrand</a> <span style=\"color:green\">&#x2B24;</span><br/><a href=\"langelier.html\">Langelier</a> <span style=\"color:green\">&#x2B24;</span><br/><a href=\"cadillac.html\">Cadillac</a> <span style=\"color:green\">&#x2B24;</span><br/><a href=\"frontenac.html\">Frontenac</a> <span style=\"color:green\">&#x2B24;</span></td></tr></li>"+
+			"<li><tr><td>432</td><td>Saint-L&eacute;onard</td></tr></li>"+
+			"<li><tr><td>444</td><td>C&eacute;gep Marie-Victorin</td></tr></li>"+
+			"<li><tr><td>448</td><td>Rivi&egrave;re-des-Prairies / Pointe-aux-Trembles</td></tr></li>"+
+			"<li><tr><td>449</td><td>Rivi&egrave;re-des-Prairies / Pointe-aux-Trembles</td></tr></li>"+
+			"<li><tr><td>EXO</td><td>Boucherville<br/>Beloeil<br/>Varennes<br/>Sainte-Julie</td></tr></li>"+
+			"<li><tr><td>RTL 61</td><td>Boucherville</td></tr></li>"+
+			"<li><tr><td>RTL 461</td><td>Boucherville</td></tr></li>"+
+			"<li><tr><td>STL 925</td><td>Laval</td></tr></li>"+
+			"<li><tr><td>CRT Lanaudi&egrave;re</td><td>Joliette</td></tr></li>"+
+			"<li><tr><td>MRC De L&#39;Assomption</td><td>L&#39;Assomption</td></tr></li>"+
+			"<li><tr><td>MRC Les Moulins</td><td>Mascouche</td></tr></li>;"
+			break;
+		}
+	}
 }
 
 window.onload = init; 
