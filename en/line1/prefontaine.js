ee0aac7075007b906954c0b235a66d5735e36100
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2022-04-17 15:55:46 -0400

    Update prefontaine.js

--- en/line1/prefontaine.js
+++ en/line1/prefontaine.js
@@ -5,7 +5,8 @@ function init()
 	{name: "Institut Teccart", type: "College", address: "3030 Rue Hochelaga", site: "https://www.teccart.qc.ca/index_en.php"},
 	{name: "Coll&egrave;ge Ville-Marie", type: "School", address: "2850 Rue Sherbrooke Est", site: "https://www.cvmarie.qc.ca/"},
 	{name: "L&#39;&Eacute;glise &agrave; Montr&eacute;al", type: "Church", address: "3201 Rue Hochelaga", site: "http://churchinmontreal.ca/"},
-	{name: "&Eacute;cole Notre-Dame-de-l&#39;Assomption", type: "School", address: "2430 Rue Darling", site: "https://notre-dame-de-lassomption.cssdm.gouv.qc.ca/"}];
+	{name: "&Eacute;cole Notre-Dame-de-l&#39;Assomption", type: "School", address: "2430 Rue Darling", site: "https://notre-dame-de-lassomption.cssdm.gouv.qc.ca/"},
+	{name: "&Eacute;glise Nativit&eacute;-de-la-Sainte-Vierge", type: "Church", address: "1855 Rue D&eacute;z&eacute;ry", site: "https://nativite150.wixsite.com/site"}];
 	
 	if(spots.length == 1){
 		document.getElementById("hotspot-js").innerHTML += "<span style=\"font-size: 14pt\"><b>"+spots.length+ " hotspot is near this station.</b></span><br/><br/>";
