f7b715bf831ca41c494ebe541cbb8821ec50b175
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2022-05-08 13:58:15 -0400

    Update cadillac.js

--- en/line1/cadillac.js
+++ en/line1/cadillac.js
@@ -9,7 +9,8 @@ function init()
 	{name: "&Eacute;cole Notre-Dame-des-Victoires", type: "School", address: "2720 Rue Bossuet", site: "https://notre-dame-des-victoires.csdm.ca/"},
 	{name: "Sanctuaire Marie-Reine-des-Coeurs", type: "Church", address: "5875 Rue Sherbrooke Est", site: "https://smrdc.org/"},
 	{name: "Parc Bossuet", type: "Park", address: "", site: "https://montreal.ca/en/places/parc-bossuet"},
-	{name: "Association Pulmonaire du Qu&eacute;bec", type: "Agency", address: "6070 Rue Sherbrooke Est", site: "https://poumonquebec.ca/en/"}];
+	{name: "Association Pulmonaire du Qu&eacute;bec", type: "Agency", address: "6070 Rue Sherbrooke Est", site: "https://poumonquebec.ca/en/"},
+	{name: "Services des Loisirs Saint-Fabien", type: "Cultural Centre", address: "5005 Rue du Quesne", site: "https://loisirsst-fabien.wixsite.com/accueil"}];
 	
 	if(spots.length == 1){
 		document.getElementById("hotspot-js").innerHTML += "<span style=\"font-size: 14pt\"><b>"+spots.length+ " hotspot is near this station.</b></span><br/><br/>";
