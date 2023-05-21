e7ff1b6079bb9c2064a923ab91607c21f9664859
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2023-05-21 14:43:13 -0400

    Update rosemont.js

--- en/line2/rosemont.js
+++ en/line2/rosemont.js
@@ -10,7 +10,8 @@ function init()
 	{name: "Parc du P&egrave;re-Marquette", type: "Park", address: "1602 Rue de Bellechasse", site: "https://montreal.ca/en/places/parc-du-pere-marquette"},
 	{name: "Centre P&egrave;re-Marquette", type: "Cultural Centre", address: "1600 Rue de Drucourt", site: "http://centreperemarquette.ca/"},
 	{name: "Ar&eacute;na P&egrave;re-Marquette", type: "Sports", address: "1605 Rue de Bellechasse", site: "https://montreal.ca/en/places/arena-pere-marquette"},
-	{name: "&Eacute;cole P&egrave;re-Marquette", type: "School", address: "6030 Rue Marquette", site: "https://pere-marquette.cssdm.gouv.qc.ca/"}];
+	{name: "&Eacute;cole P&egrave;re-Marquette", type: "School", address: "6030 Rue Marquette", site: "https://pere-marquette.cssdm.gouv.qc.ca/"},
+	{name: "&Eacute;cole des M&eacute;tiers de l'&Eacute;quipement Motoris&eacute; de Montr&eacute;al", type: "Trade School", address: "5455 Rue Saint-Denis", site: "https://ecole-metiers-motorise.cssdm.gouv.qc.ca"}];
 	
 	if(spots.length == 1){
 		document.getElementById("hotspot-js").innerHTML += "<span style=\"font-size: 14pt\"><b>"+spots.length+ " hotspot is near this station.</b></span><br/><br/>";
