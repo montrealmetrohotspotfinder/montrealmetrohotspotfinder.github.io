db154f16cf51f12e39403cf2062d54f57e2b7bd7
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2024-01-26 00:23:11 -0500

    Update search.js

--- en/search.js
+++ en/search.js
@@ -994,7 +994,8 @@ function init(){
 	{name: "Restaurant Sauvage", type: "Restaurant", address: "52 Rue Saint-Jacques", site: "https://www.restaurantsauvage.com/"},
 	{name: "Humaniti H&ocirc;tel Montr&eacute;al", type: "Hotel", address: "340 Rue de la Gaucheti&egrave;re Ouest", site: "https://www.humanitihotel.com/"},
 	{name: "Hayat", type: "Restaurant", address: "131 Rue de la Commune Ouest", site: "https://hayatmontreal.com"},
-	{name: "Stash Caf&eacute;", type: "Restaurant", address: "200 Rue Saint-Paul Ouest", site: "https://restaurantstashcafe.ca/en/home-english/"}];
+	{name: "Stash Caf&eacute;", type: "Restaurant", address: "200 Rue Saint-Paul Ouest", site: "https://restaurantstashcafe.ca/en/home-english/"},
+	{name: "Restaurant Gandhi", type: "Restaurant", address: "230 Rue Saint-Paul Ouest", site: "#"}];
 	
 	SquareVictoria = [{name: "Old Montreal", type: "District", address: "", site: "https://vieuxmontreal.ca/en/"},
 	{name: "Old Port of Montreal", type: "Port", address: "333 Rue de la Commune Ouest", site: "https://www.oldportofmontreal.com/"},
