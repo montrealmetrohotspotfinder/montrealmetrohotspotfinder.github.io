dfe33bbf8df4c92d6bb3fdda3dcd7fac2b0c9a5d
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2024-02-03 18:08:17 -0500

    Update place-saint-henri.js

--- fr/ligne2/place-saint-henri.js
+++ fr/ligne2/place-saint-henri.js
@@ -2,20 +2,22 @@ function init()
 {
 	hnames = [ "Parc Sir-Georges-&Eacute;tienne-Cartier", "Parc Louis-Cyr", "Institut Technique Aviron", "Parc Saint-Henri", "Piscine Saint-Henri", "CLSC Saint-Henri", 
 	"&Eacute;cole des M&eacute;tiers du Sud-Ouest-de-Montr&eacute;al", "Parc &Eacute;mile-Berliner", "&Eacute;cole Ludger-Duvernay", 
-	"Arthurs Nosh Bar", "&Eacute;glise Saint-Zotique", "Tacos Frida", "Biblioth&egrave;que Saint-Henri", "Eva&#39;s", "Gite L&#39;Impr&eacute;vu" ];
+	"Arthurs Nosh Bar", "&Eacute;glise Saint-Zotique", "Tacos Frida", "Biblioth&egrave;que Saint-Henri", "Eva&#39;s", "Gite L&#39;Impr&eacute;vu",
+	"Parc de la Ferme-Brodie", "Parc des Selliers", "Parc Lacasse", "Parc des Couturi&egrave;res" ];
 	
 	htypes = [ "Parc", "Parc", "&Eacute;cole de M&eacute;tiers", "Parc", "Piscine", "Agence", "&Eacute;cole de M&eacute;tiers", "Parc", "&Eacute;cole",
-	"Restaurant", "&Eacute;glise", "Restaurant", "Biblioth&egrave;que", "Restaurant", "H&ocirc;tel" ];
+	"Restaurant", "&Eacute;glise", "Restaurant", "Biblioth&egrave;que", "Restaurant", "H&ocirc;tel", "Parc", "Parc", "Parc", "Parc" ];
 	
 	haddresses = [ "127 Rue du Square-Sir-George-&Eacute;tienne-Cartier", "167 Rue Saint-Ferdinand", "5460 Avenue Royalmount", "753 Rue Agn&egrave;s", "4055 Rue Saint-Jacques", 
 	"3833 Rue Notre-Dame Ouest", "717 Rue Saint-Ferdinand", "", "770 Rue du Couvent", "4621 Rue Notre-Dame Ouest", "4561 Rue Notre-Dame Ouest", "4350 Rue Notre-Dame Ouest", 
-	"4707 Rue Notre-Dame Ouest", "3981 Rue Notre-Dame Ouest", "3932 Rue Saint-Jacques" ];
+	"4707 Rue Notre-Dame Ouest", "3981 Rue Notre-Dame Ouest", "3932 Rue Saint-Jacques", "3977 Rue Saint-Jacques", "712 Rue Lacasse", "", "" ];
 	
 	hsites = [ "https://montreal.ca/lieux/square-sir-george-etienne-cartier", "https://montreal.ca/lieux/parc-louis-cyr", "http://www.avirontech.com/fr", 
 	"https://montreal.ca/lieux/parc-saint-henri", "https://montreal.ca/lieux/piscine-saint-henri", "https://ciusss-centresudmtl.gouv.qc.ca/etablissement/clsc-de-saint-henri",
 	"https://ecole-metiers-sud-ouest.csdm.ca/", "https://montreal.ca/lieux/parc-emile-berliner", "https://ludger-duvernay.cssdm.gouv.qc.ca/",
 	"http://arthursmtl.com/fr/accueil/", "https://eglise-saint-zotique.business.site/", "https://www.tacosfridamtl.ca/?lang=fr", "https://montreal.ca/lieux/bibliotheque-saint-henri",
-	"#", "https://www.imprevu.org/index.html" ];
+	"#", "https://www.imprevu.org/index.html", "https://montreal.ca/lieux/parc-de-la-ferme-brodie", "https://montreal.ca/lieux/parc-des-selliers", "https://montreal.ca/lieux/parc-lacasse", 
+	"https://montreal.ca/lieux/parc-des-couturieres" ];
 	
 	if(hnames.length == 1){
 		document.getElementById("hotspot-js").innerHTML += "<span style=\"font-size: 14pt\"><em lang=\"fr\"><b>"+hnames.length+ " point d&#39;int&eacute;r&ecirc;t est &agrave; proximit&eacute; de cette station.</b></em></span><br/><br/>";
