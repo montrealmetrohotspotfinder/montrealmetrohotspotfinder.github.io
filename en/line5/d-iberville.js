6574ef4c5b9a657b79d8447ee2d83533eaf63263
montrealmetrohotspotfinder <82291095+montrealmetrohotspotfinder@users.noreply.github.com>
2024-02-04 23:12:50 -0500

    Update d-iberville.js

--- en/line5/d-iberville.js
+++ en/line5/d-iberville.js
@@ -6,7 +6,8 @@ function init()
 	{name: "&Eacute;glise Biblique Pierre Angulaire", type: "Church", address: "2200 Rue Jean-Talon Est", site: "#"},
 	{name: "Parc Molson", type: "Park", address: "2473 Rue Beaubien Est", site: "https://montreal.ca/en/places/parc-molson"},
 	{name: "Paroisse Saint-Mathieu", type: "Church", address: "2600 Rue Jean-Talon Est", site: "https://www.diocesemontreal.org/en/lets-connect/churches-parishes/saint-mathieu"},
-	{name: "Paroisse Saint-Barth&eacute;lemy", type: "Church", address: "7137 Avenue des &Eacute;rables", site: "https://diocesemontreal.org/en/lets-connect/churches-parishes/saint-barthelemy"}];
+	{name: "Paroisse Saint-Barth&eacute;lemy", type: "Church", address: "7137 Avenue des &Eacute;rables", site: "https://diocesemontreal.org/en/lets-connect/churches-parishes/saint-barthelemy"},
+	{name: "Club L", type: "Restaurant", address: "2570 Rue Jean-Talon Est", site: "https://www.leclubl.com"}];
 	
 	if(spots.length == 1){
 		document.getElementById("hotspot-js").innerHTML += "<span style=\"font-size: 14pt\"><b>"+spots.length+ " hotspot is near this station.</b></span><br/><br/>";
